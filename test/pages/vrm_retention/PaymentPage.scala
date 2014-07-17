c8d17ff8fb92fa925f0cce226b6fbc4a1ff490b4
Geraint Jones <geraint@Geraints-MacBook-Air.local>
2014-07-17 11:43:38 +0100

    more testing

---
+++ test/pages/vrm_retention/PaymentPage.scala
@@ -0,0 +1,12 @@
+package pages.vrm_retention
+
+import helpers.webbrowser.{Element, Page, WebBrowserDSL, WebDriverFactory}
+import mappings.vrm_retention.Confirm.NextId
+import org.openqa.selenium.WebDriver
+
+object PaymentPage extends Page with WebBrowserDSL {
+  final val address = "/vrm-retention/payment"
+  override val url: String = WebDriverFactory.testUrl + address.substring(1)
+  final override val title: String = "Take a Registration Number off a Vehicle"
+
+}
