bbcc3ddde914754f9c2ff988a9419ffc9950b024
Geraint Jones <geraint@Geraints-MacBook-Air.local>
2014-07-17 11:30:08 +0100

    started a confirm int spec

--- test/pages/vrm_retention/ConfirmPage.scala
+++ test/pages/vrm_retention/ConfirmPage.scala
@@ -5,5 +5,6 @@ import helpers.webbrowser.{Page, WebBrowserDSL, WebDriverFactory}
 object ConfirmPage extends Page with WebBrowserDSL {
   final val address = "/vrm-retention/confirm"
   override val url: String = WebDriverFactory.testUrl + address.substring(1)
-  final override val title: String = "Take a Registration Number off a Vehicle"
+  final override val title: String = "Confirm details"
+
 }
---
+++ test/views/vrm_retention/ConfirmIntegrationSpec.scala
@@ -0,0 +1,132 @@
+package views.vrm_retention
+
+import helpers.tags.UiTag
+import helpers.UiSpec
+import helpers.webbrowser.TestHarness
+import pages.vrm_retention._
+import org.openqa.selenium.{By, WebElement, WebDriver}
+import helpers.vrm_retention.CookieFactoryForUISpecs
+
+final class ConfirmIntegrationSpec extends UiSpec with TestHarness {
+
+  "go to page" should {
+    "display the page" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+
+      cacheSetup()
+
+      go to ConfirmPage
+
+      page.url should equal(ConfirmPage.url)
+    }
+
+    "contain the hidden csrfToken field" taggedAs UiTag in new WebBrowser {
+      go to VehicleLookupPage
+      val csrf: WebElement = webDriver.findElement(By.name(filters.csrf_prevention.CsrfPreventionAction.TokenName))
+      csrf.getAttribute("type") should equal("hidden")
+      csrf.getAttribute("name") should equal(filters.csrf_prevention.CsrfPreventionAction.TokenName)
+      csrf.getAttribute("value").size > 0 should equal(true)
+    }
+  }
+
+//  "findVehicleDetails button" should {
+//
+//    "redirect to ConfirmPage when valid submission and current keeper" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(isCurrentKeeper = true)
+//
+//      page.url should equal(ConfirmPage.url)
+//    }
+//
+//    "redirect to SetupBusinessDetailsPage when valid submission and not current keeper" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(isCurrentKeeper = false)
+//
+//      page.url should equal(SetupBusinessDetailsPage.url)
+//    }
+//
+//    "display one validation error message when no referenceNumber is entered" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(referenceNumber = "")
+//
+//      ErrorPanel.numberOfErrors should equal(1)
+//    }
+//
+//    "display one validation error message when no registrationNumber is entered" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(registrationNumber = "")
+//
+//      ErrorPanel.numberOfErrors should equal(1)
+//    }
+//
+//    "display one validation error message when a registrationNumber is entered containing one character" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(registrationNumber = "a")
+//
+//      ErrorPanel.numberOfErrors should equal(1)
+//    }
+//
+//    "display one validation error message when a registrationNumber is entered containing special characters" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(registrationNumber = "$^")
+//
+//      ErrorPanel.numberOfErrors should equal(1)
+//    }
+//
+//    "display two validation error messages when no vehicle details are entered but consent is given" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(referenceNumber = "", registrationNumber = "")
+//
+//      ErrorPanel.numberOfErrors should equal(2)
+//    }
+//
+//    "display one validation error message when only a valid referenceNumber is entered and consent is given" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(registrationNumber = "")
+//
+//      ErrorPanel.numberOfErrors should equal(1)
+//    }
+//
+//    "display one validation error message when only a valid registrationNumber is entered and consent is given" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      happyPath(referenceNumber = "")
+//
+//      ErrorPanel.numberOfErrors should equal(1)
+//    }
+//
+//    "redirect to vrm locked when too many attempting to lookup a locked vrm" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      tryLockedVrm()
+//
+//      page.url should equal(VrmLockedPage.url)
+//    }
+//  }
+//
+//  "back" should {
+//    "display previous page when back link is clicked" taggedAs UiTag in new WebBrowser {
+//      go to BeforeYouStartPage
+//
+//      go to VehicleLookupPage
+//
+//      click on back
+//
+//      page.url should equal(BeforeYouStartPage.url)
+//    }
+//  }
+
+  private def cacheSetup()(implicit webDriver: WebDriver) =
+    CookieFactoryForUISpecs.
+      vehicleDetailsModel().
+      keeperDetailsModel().
+      businessDetails()
+}
