ba02f8c5436f6a9922c7374b82dc5b663f854f0d
Scott Walker <scott.walker@valtech.co.uk>
2014-08-12 10:43:09 +0100

    US951: Commonised BaseTemplate

--- app/utils/helpers/BaseTemplate.scala
+++
@@ -1,7 +0,0 @@
-package utils.helpers
-
-import views.html.helper.FieldConstructor
-
-object BaseTemplate {
-  implicit val fieldConstructor = FieldConstructor(views.html.helper.templates.baseTemplate.f)
-}
