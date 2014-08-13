880ae1c9b2d9c2f2a8b0f575704683b91b1874aa
Scott Walker <scott.walker@valtech.co.uk>
2014-08-13 16:33:06 +0100

    US951: commonised baseTemplate and emptyTemplate, added TODOs

--- app/mappings/common/UPRN.scala
+++ app/mappings/common/UPRN.scala
@@ -3,6 +3,7 @@ package mappings.common
 import play.api.data.Forms.{number, optional}
 import play.api.data.Mapping
 
+// TODO move to common project except for the ID
 object Uprn {
   final val UprnId = "uprn"
 
