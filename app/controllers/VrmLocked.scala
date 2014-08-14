8e2a837f7bd12c137ffa8d5f6dd2fd39462e3dc5
Scott Walker <scott.walker@valtech.co.uk>
2014-08-14 11:28:33 +0100

    Refactor: apply method on view model

--- app/controllers/VrmLocked.scala
+++ app/controllers/VrmLocked.scala
@@ -2,14 +2,13 @@ package controllers
 
 import com.google.inject.Inject
 import mappings.vrm_retention.RelatedCacheKeys
-import viewmodels.{VehicleAndKeeperDetailsModel, VehicleAndKeeperLookupFormModel, VrmLockedViewModel}
 import play.api.Logger
 import play.api.mvc.{Action, Controller}
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.{RichCookies, RichSimpleResult}
 import uk.gov.dvla.vehicles.presentation.common.model.BruteForcePreventionModel
-import uk.gov.dvla.vehicles.presentation.common.views.constraints.RegistrationNumber.formatVrm
 import utils.helpers.Config
+import viewmodels.{VehicleAndKeeperDetailsModel, VehicleAndKeeperLookupFormModel, VrmLockedViewModel}
 
 final class VrmLocked @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                   config: Config) extends Controller {
@@ -21,10 +20,10 @@ final class VrmLocked @Inject()()(implicit clientSideSessionFactory: ClientSideS
         request.cookies.getModel[VehicleAndKeeperDetailsModel]) match {
         case (Some(bruteForcePreventionViewModel), Some(vehicleAndKeeperLookupFormModel), Some(vehicleAndKeeperDetails)) =>
           Logger.debug(s"VrmLocked - Displaying the vrm locked error page")
-          Ok(views.html.vrm_retention.vrm_locked(createViewModel(vehicleAndKeeperDetails), bruteForcePreventionViewModel.dateTimeISOChronology))
+          Ok(views.html.vrm_retention.vrm_locked(VrmLockedViewModel(vehicleAndKeeperDetails), bruteForcePreventionViewModel.dateTimeISOChronology))
         case (Some(bruteForcePreventionViewModel), Some(vehicleAndKeeperLookupFormModel), None) =>
           Logger.debug(s"VrmLocked - Displaying the vrm locked error page")
-          Ok(views.html.vrm_retention.vrm_locked(createViewModel(vehicleAndKeeperLookupFormModel), bruteForcePreventionViewModel.dateTimeISOChronology))
+          Ok(views.html.vrm_retention.vrm_locked(VrmLockedViewModel(vehicleAndKeeperLookupFormModel), bruteForcePreventionViewModel.dateTimeISOChronology))
         case _ =>
           Logger.debug("VrmLocked - Can't find cookies")
           Redirect(routes.VehicleLookup.present())
@@ -35,16 +34,4 @@ final class VrmLocked @Inject()()(implicit clientSideSessionFactory: ClientSideS
     implicit request =>
       Redirect(routes.MockFeedback.present()).discardingCookies(RelatedCacheKeys.FullSet)
   }
-
-  private def createViewModel(vehicleAndKeeperDetails: VehicleAndKeeperDetailsModel): VrmLockedViewModel = // TODO can be moved to an apply function on a companion object.
-    VrmLockedViewModel(
-      registrationNumber = vehicleAndKeeperDetails.registrationNumber,
-      vehicleMake = vehicleAndKeeperDetails.vehicleMake,
-      vehicleModel = vehicleAndKeeperDetails.vehicleModel)
-
-  private def createViewModel(vehicleAndKeeperLookupFormModel: VehicleAndKeeperLookupFormModel): VrmLockedViewModel = // TODO can be moved to an apply function on a companion object.
-    VrmLockedViewModel(
-      registrationNumber = formatVrm(vehicleAndKeeperLookupFormModel.registrationNumber),
-      vehicleMake = None,
-      vehicleModel = None)
 }
