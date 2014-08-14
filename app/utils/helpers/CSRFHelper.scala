77033513196f2597a808b66d5e2b5b296ed0cf6a
Scott Walker <scott.walker@valtech.co.uk>
2014-08-14 08:56:46 +0100

    Refactor: shortened name in html by making an import

--- app/utils/helpers/CSRFHelper.scala
+++ app/utils/helpers/CSRFHelper.scala
@@ -2,12 +2,12 @@ package utils.helpers
 
 import play.api.templates.{Html, HtmlFormat}
 import uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.CsrfPreventionToken
+import uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName
 
 object CsrfHelper {
 
   def hiddenFormField(implicit token: CsrfPreventionToken, config: utils.helpers.Config): Html =
     if (config.isCsrfPreventionEnabled) {
-      val csrfTokenName = uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName
-      Html( s"""<input type="hidden" name="$csrfTokenName" value="${HtmlFormat.escape(token.value)}"/>""")
+      Html( s"""<input type="hidden" name="$TokenName" value="${HtmlFormat.escape(token.value)}"/>""")
     } else Html.empty
 }
