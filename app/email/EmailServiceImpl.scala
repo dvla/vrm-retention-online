842ec4df7461d1828b90f19ecf8f4df8b7ab00a1
Geraint Jones <geraint.jones@valtech.co.uk>
2014-08-18 16:45:45 +0100

    updated email to use commons email

--- app/email/EmailServiceImpl.scala
+++ app/email/EmailServiceImpl.scala
@@ -41,20 +41,18 @@ final class EmailServiceImpl @Inject()(dateService: DateService, config: Config)
       mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822")
       CommandMap.setDefaultCommandMap(mc)
 
-      val mail = use[MailerPlugin].email
-
-      mail.setSubject("Your Retention of Registration Number " + vrm)
-      mail.setRecipient(emailAddress)
-      mail.setFrom(senderEmailAddress)
-//      mail.sendHtml("<html>test message</html>") // TODO html payload
-//      mail.send("VRM: " + vrm + NEW_LINE +
-//                "Retention Cert Id: " + retentionCertId + NEW_LINE +
-//                "Transaction Id: " + transactionId + NEW_LINE +
-//                "Transaction Timestamp: " + transactionTimestamp + NEW_LINE +
-//                "Keeper Name: " + getKeeperName(keeperTitle, keeperFirstName, keeperLasttName) + NEW_LINE +
-//                "Keeper Address: " + getKeeperAddress(keeperAddress) + NEW_LINE +
-//                "Amount Debited: " + amountDebited + NEW_LINE +
-//                "Replacement VRM: " + replacementVrm
+//      send a new Mail (
+//        from = (senderEmailAddress, "Viv Richards"),
+//        to = emailAddress,
+//        subject = "Your Retention of Registration Number " + vrm,
+//        message = "VRM: " + vrm + NEW_LINE +
+//                  "Retention Cert Id: " + retentionCertId + NEW_LINE +
+//                  "Transaction Id: " + transactionId + NEW_LINE +
+//                  "Transaction Timestamp: " + transactionTimestamp + NEW_LINE +
+//                  "Keeper Name: " + getKeeperName(keeperTitle, keeperFirstName, keeperLasttName) + NEW_LINE +
+//                  "Keeper Address: " + getKeeperAddress(keeperAddress) + NEW_LINE +
+//                  "Amount Debited: " + amountDebited + NEW_LINE +
+//                  "Replacement VRM: " + replacementVrm
 //      )
 
       Logger.debug("Email sent to " + emailAddress)
@@ -77,10 +75,63 @@ final class EmailServiceImpl @Inject()(dateService: DateService, config: Config)
     var keeperAddress = ""
     if (address.isDefined) {
       for (addressLine <- address.get.address) {
-        Logger.debug(addressLine)
         keeperAddress = keeperAddress + NEW_LINE + addressLine
       }
     }
     keeperAddress
+  }
+
+  sealed abstract class MailType
+  case object Plain extends MailType
+  case object Rich extends MailType
+  case object MultiPart extends MailType
+
+  case class Mail(
+                   from: (String, String), // (email -> name)
+                   to: Seq[String],
+                   cc: Seq[String] = Seq.empty,
+                   bcc: Seq[String] = Seq.empty,
+                   subject: String,
+                   message: String,
+                   richMessage: Option[String] = None,
+                   attachment: Option[(java.io.File)] = None
+                   )
+
+  object send {
+    def a(mail: Mail) {
+      import org.apache.commons.mail._
+
+      val format =
+        if (mail.attachment.isDefined) MultiPart
+        else if (mail.richMessage.isDefined) Rich
+        else Plain
+
+      val commonsMail: Email = format match {
+        case Plain => new SimpleEmail().setMsg(mail.message)
+        case Rich => new HtmlEmail().setHtmlMsg(mail.richMessage.get).setTextMsg(mail.message)
+        case MultiPart => {
+          val attachment = new EmailAttachment()
+          attachment.setPath(mail.attachment.get.getAbsolutePath)
+          attachment.setDisposition(EmailAttachment.ATTACHMENT)
+          attachment.setName(mail.attachment.get.getName)
+          new MultiPartEmail().attach(attachment).setMsg(mail.message)
+        }
+      }
+
+      // Can't add these via fluent API because it produces exceptions
+      mail.to foreach (commonsMail.addTo(_))
+      mail.cc foreach (commonsMail.addCc(_))
+      mail.bcc foreach (commonsMail.addBcc(_))
+
+      commonsMail.setHostName(config.emailSmtpHost)
+      commonsMail.setSmtpPort(config.emailSmtpPort)
+      commonsMail.setStartTLSEnabled(true)
+      commonsMail.setAuthentication(config.emailSmtpUser,config.emailSmtpPassword)
+
+      commonsMail.
+        setFrom(mail.from._1, mail.from._2).
+        setSubject(mail.subject).
+        send()
+    }
   }
 }
