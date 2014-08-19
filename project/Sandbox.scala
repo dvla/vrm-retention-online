9c3dd94be90ca083df0f2a7ec2b60b44b425598b
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-08-19 11:30:46 +0100

    Add the typesafe repository so the sandbox can resolve the services deps

--- project/Sandbox.scala
+++ project/Sandbox.scala
@@ -10,6 +10,7 @@ object CommonResolvers {
   val nexus = "http://rep002-01.skyscape.preview-dvla.co.uk:8081/nexus/content/repositories"
 
   val projectResolvers = Seq(
+    "typesafe repo" at "http://repo.typesafe.com/typesafe/releases",
     "spray repo" at "http://repo.spray.io/",
     "local nexus snapshots" at s"$nexus/snapshots",
     "local nexus releases" at s"$nexus/releases"
