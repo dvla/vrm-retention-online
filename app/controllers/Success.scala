de107117cfff951d95d6d8bcffe5936bec8ce2a4
Viv Richards <vivrichards600@hotmail.com>
2014-08-22 10:42:07 +0100

    US1017 - temp link added to success page to enable Tom /UI to be able to style the email output a bit easier by viewing its contents in a browser rather than have to send and open emails each time.

--- app/controllers/Success.scala
+++ app/controllers/Success.scala
@@ -3,21 +3,21 @@ package controllers
 import java.io.ByteArrayInputStream
 import com.google.inject.Inject
 import email.EmailService
-import viewmodels._
 import pdf.PdfService
 import play.api.libs.iteratee.Enumerator
 import play.api.mvc._
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.{RichCookies, RichResult}
 import utils.helpers.Config
+import viewmodels._
+import views.vrm_retention.Confirm._
+import views.vrm_retention.RelatedCacheKeys
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import views.vrm_retention.RelatedCacheKeys
-import views.vrm_retention.Confirm._
-import scala.Some
+
 
 final class Success @Inject()(pdfService: PdfService, emailService: EmailService)(implicit clientSideSessionFactory: ClientSideSessionFactory,
-                                                      config: Config) extends Controller {
+                                                                                  config: Config) extends Controller {
 
   def present = Action {
     implicit request =>
@@ -82,5 +82,72 @@ final class Success @Inject()(pdfService: PdfService, emailService: EmailService
     Redirect(routes.BeforeYouStart.present()).
       discardingCookies(RelatedCacheKeys.RetainSet)
     // TODO remove Business Cache if consent not sent
+  }
+
+  //TODO: This is duplicate val used within EmailServiceImpl
+  private final val amountDebited: String = "80.00"
+
+  //TODO: We do not want the user to be able to get to this page - added for Tom to be able to style email easier
+  def previewEmail = Action { implicit request =>
+    (request.cookies.getModel[VehicleAndKeeperDetailsModel],
+      request.cookies.getModel[EligibilityModel], request.cookies.getModel[BusinessDetailsModel],
+      request.cookies.getString(KeeperEmailCacheKey), request.cookies.getModel[RetainModel]) match {
+
+      case (Some(vehicleAndKeeperDetails), Some(eligibilityModel), Some(businessDetailsModel), Some(keeperEmail), Some(retainModel)) =>
+        Ok(views.html.vrm_retention.email_template(vehicleAndKeeperDetails.registrationNumber,
+          retainModel.certificateNumber,
+          retainModel.transactionId,
+          retainModel.transactionTimestamp,
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          amountDebited,
+          eligibilityModel.replacementVRM))
+
+
+      case (Some(vehicleAndKeeperDetails), Some(eligibilityModel), Some(businessDetailsModel), None, Some(retainModel)) =>
+        Ok(views.html.vrm_retention.email_template(vehicleAndKeeperDetails.registrationNumber,
+          retainModel.certificateNumber,
+          retainModel.transactionId,
+          retainModel.transactionTimestamp,
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          amountDebited,
+          eligibilityModel.replacementVRM))
+
+      case (Some(vehicleAndKeeperDetails), Some(eligibilityModel), None, Some(keeperEmail), Some(retainModel)) =>
+        Ok(views.html.vrm_retention.email_template(vehicleAndKeeperDetails.registrationNumber,
+          retainModel.certificateNumber,
+          retainModel.transactionId,
+          retainModel.transactionTimestamp,
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          amountDebited,
+          eligibilityModel.replacementVRM))
+
+      case (Some(vehicleAndKeeperDetails), Some(eligibilityModel), None, None, Some(retainModel)) =>
+        Ok(views.html.vrm_retention.email_template(vehicleAndKeeperDetails.registrationNumber,
+          retainModel.certificateNumber,
+          retainModel.transactionId,
+          retainModel.transactionTimestamp,
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          formatKeeperAddress(vehicleAndKeeperDetails),
+          amountDebited,
+          eligibilityModel.replacementVRM))
+
+      case _ =>
+        Redirect(routes.MicroServiceError.present())
+    }
+  }
+
+  //TODO: This is duplicate code used within EmailServiceImpl
+  def formatKeeperName(vehicleAndKeeperDetailsModel: VehicleAndKeeperDetailsModel): String = {
+    Seq(vehicleAndKeeperDetailsModel.title, vehicleAndKeeperDetailsModel.firstName, vehicleAndKeeperDetailsModel.lastName).
+      flatten.
+      mkString(" ")
+  }
+
+  //TODO: This is duplicate code used within EmailServiceImpl
+  def formatKeeperAddress(vehicleAndKeeperDetailsModel: VehicleAndKeeperDetailsModel): String = {
+    vehicleAndKeeperDetailsModel.address.get.address.mkString(",")
   }
 }
