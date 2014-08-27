bb5d5c36daaa06bc33039faee040705c6dbf584f
Geraint Jones <geraint.jones@valtech.co.uk>
2014-08-27 08:31:33 +0100

    add transaction id ot the failure pages

--- app/controllers/VehicleLookupFailure.scala
+++ app/controllers/VehicleLookupFailure.scala
@@ -8,20 +8,21 @@ import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicit
 import uk.gov.dvla.vehicles.presentation.common.model.BruteForcePreventionModel
 import utils.helpers.Config
 import viewmodels.{VehicleAndKeeperDetailsModel, VehicleAndKeeperLookupFormModel, VehicleLookupFailureViewModel}
-import views.vrm_retention.VehicleLookup.VehicleAndKeeperLookupResponseCodeCacheKey
+import views.vrm_retention.VehicleLookup._
 
 final class VehicleLookupFailure @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                              config: Config) extends Controller {
 
   def present = Action { implicit request =>
-    (request.cookies.getModel[BruteForcePreventionModel],
+    (request.cookies.getString(TransactionIdCacheKey),
+      request.cookies.getModel[BruteForcePreventionModel],
       request.cookies.getModel[VehicleAndKeeperLookupFormModel],
       request.cookies.getModel[VehicleAndKeeperDetailsModel],
       request.cookies.getString(VehicleAndKeeperLookupResponseCodeCacheKey)) match {
-      case (Some(bruteForcePreventionResponse), Some(vehicleAndKeeperLookupFormModel), Some(vehicleAndKeeperDetails), Some(vehicleLookupResponseCode)) =>
-        displayVehicleLookupFailure(vehicleAndKeeperLookupFormModel, bruteForcePreventionResponse, Some(vehicleAndKeeperDetails), vehicleLookupResponseCode)
-      case (Some(bruteForcePreventionResponse), Some(vehicleAndKeeperLookupFormModel), None, Some(vehicleLookupResponseCode)) =>
-        displayVehicleLookupFailure(vehicleAndKeeperLookupFormModel, bruteForcePreventionResponse, None, vehicleLookupResponseCode)
+      case (Some(transactionId), Some(bruteForcePreventionResponse), Some(vehicleAndKeeperLookupFormModel), Some(vehicleAndKeeperDetails), Some(vehicleLookupResponseCode)) =>
+        displayVehicleLookupFailure(transactionId, vehicleAndKeeperLookupFormModel, bruteForcePreventionResponse, Some(vehicleAndKeeperDetails), vehicleLookupResponseCode)
+      case (Some(transactionId), Some(bruteForcePreventionResponse), Some(vehicleAndKeeperLookupFormModel), None, Some(vehicleLookupResponseCode)) =>
+        displayVehicleLookupFailure(transactionId, vehicleAndKeeperLookupFormModel, bruteForcePreventionResponse, None, vehicleLookupResponseCode)
       case _ => Redirect(routes.BeforeYouStart.present())
     }
   }
@@ -35,7 +36,8 @@ final class VehicleLookupFailure @Inject()()(implicit clientSideSessionFactory: 
     }
   }
 
-  private def displayVehicleLookupFailure(vehicleAndKeeperLookupFormModel: VehicleAndKeeperLookupFormModel,
+  private def displayVehicleLookupFailure(transactionId: String,
+                                          vehicleAndKeeperLookupFormModel: VehicleAndKeeperLookupFormModel,
                                           bruteForcePreventionViewModel: BruteForcePreventionModel,
                                           vehicleAndKeeperDetails: Option[VehicleAndKeeperDetailsModel],
                                           vehicleAndKeeperLookupResponseCode: String)(implicit request: Request[AnyContent]) = {
@@ -45,6 +47,7 @@ final class VehicleLookupFailure @Inject()()(implicit clientSideSessionFactory: 
     }
 
     Ok(views.html.vrm_retention.vehicle_lookup_failure(
+      transactionId = transactionId,
       vehicleLookupFailureViewModel = vehicleLookupFailureViewModel,
       data = vehicleAndKeeperLookupFormModel,
       responseCodeVehicleLookupMSErrorMessage = vehicleAndKeeperLookupResponseCode,
--- app/controllers/VrmLocked.scala
+++ app/controllers/VrmLocked.scala
@@ -10,21 +10,24 @@ import play.api.mvc.{Action, Controller}
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.{RichCookies, RichResult}
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import utils.helpers.Config
+import views.vrm_retention.VehicleLookup._
+import scala.Some
 
 final class VrmLocked @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                   config: Config) extends Controller {
 
   def present = Action {
     implicit request =>
-      (request.cookies.getModel[BruteForcePreventionModel],
+      (request.cookies.getString(TransactionIdCacheKey),
+        request.cookies.getModel[BruteForcePreventionModel],
         request.cookies.getModel[VehicleAndKeeperLookupFormModel],
         request.cookies.getModel[VehicleAndKeeperDetailsModel]) match {
-        case (Some(bruteForcePreventionViewModel), Some(vehicleAndKeeperLookupFormModel), Some(vehicleAndKeeperDetails)) =>
+        case (Some(transactionId), Some(bruteForcePreventionViewModel), Some(vehicleAndKeeperLookupFormModel), Some(vehicleAndKeeperDetails)) =>
           Logger.debug(s"VrmLocked - Displaying the vrm locked error page")
-          Ok(views.html.vrm_retention.vrm_locked(VrmLockedViewModel(vehicleAndKeeperDetails), bruteForcePreventionViewModel.dateTimeISOChronology))
-        case (Some(bruteForcePreventionViewModel), Some(vehicleAndKeeperLookupFormModel), None) =>
+          Ok(views.html.vrm_retention.vrm_locked(transactionId, VrmLockedViewModel(vehicleAndKeeperDetails), bruteForcePreventionViewModel.dateTimeISOChronology))
+        case (Some(transactionId), Some(bruteForcePreventionViewModel), Some(vehicleAndKeeperLookupFormModel), None) =>
           Logger.debug(s"VrmLocked - Displaying the vrm locked error page")
-          Ok(views.html.vrm_retention.vrm_locked(VrmLockedViewModel(vehicleAndKeeperLookupFormModel), bruteForcePreventionViewModel.dateTimeISOChronology))
+          Ok(views.html.vrm_retention.vrm_locked(transactionId, VrmLockedViewModel(vehicleAndKeeperLookupFormModel), bruteForcePreventionViewModel.dateTimeISOChronology))
         case _ =>
           Logger.debug("VrmLocked - Can't find cookies")
           Redirect(routes.VehicleLookup.present())
