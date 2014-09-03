50c2d991da5b0c77b3de7e1e5924c7399894075c
Geraint Jones <geraint.jones@valtech.co.uk>
2014-09-03 08:46:03 +0100

    use Email validation from comoon

--- app/viewmodels/SetupBusinessDetailsFormModel.scala
+++ app/viewmodels/SetupBusinessDetailsFormModel.scala
@@ -1,12 +1,12 @@
 package viewmodels
 
 import views.vrm_retention.SetupBusinessDetails._
-import mappings.common.Email.email
 import play.api.data.Forms.mapping
 import play.api.data.validation.Constraints
 import play.api.libs.json.Json
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CacheKey
 import uk.gov.dvla.vehicles.presentation.common.mappings.Postcode._
+import uk.gov.dvla.vehicles.presentation.common.mappings.Email.email
 
 final case class SetupBusinessDetailsFormModel(name: String, contact: String, email: String, postcode: String)
 
