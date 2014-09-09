28fdab43e8a19ad61b60b777302b7ff292916de5
Scott Walker <scott.walker@valtech.co.uk>
2014-09-09 10:29:47 +0100

    Test Confirm page submit for writing cookies

--- test/controllers/ConfirmUnitSpec.scala
+++ test/controllers/ConfirmUnitSpec.scala
@@ -1,10 +1,14 @@
 package controllers
 
+import helpers.common.CookieHelper.fetchCookiesFromHeaders
 import helpers.vrm_retention.CookieFactoryForUnitSpecs
 import helpers.{UnitSpec, WithApplication}
-import pages.vrm_retention.VehicleLookupPage
+import pages.vrm_retention.{PaymentPage, VehicleLookupPage}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{LOCATION, OK}
+import services.fakes.AddressLookupServiceConstants.KeeperEmailValid
+import views.vrm_retention.Confirm.{StoreBusinessDetailsCacheKey, KeeperEmailCacheKey, StoreDetailsConsentId, KeeperEmailId}
+import views.vrm_retention.VehicleLookup.{UserType_Business,UserType_Keeper}
 
 final class ConfirmUnitSpec extends UnitSpec {
 
@@ -23,7 +27,6 @@ final class ConfirmUnitSpec extends UnitSpec {
         withCookies(CookieFactoryForUnitSpecs.businessDetailsModel()).
         withCookies(CookieFactoryForUnitSpecs.storeBusinessDetailsConsent("true"))
       val result = confirm.present(request)
-
       whenReady(result, timeout) { r =>
         r.header.status should equal(OK)
       }
@@ -32,7 +35,6 @@ final class ConfirmUnitSpec extends UnitSpec {
     "redirect to VehicleLookup when required cookies do not exist" in new WithApplication {
       val request = FakeRequest()
       val result = confirm.present(request)
-
       whenReady(result) { r =>
         r.header.headers.get(LOCATION) should equal(Some(VehicleLookupPage.address))
       }
@@ -41,9 +43,76 @@ final class ConfirmUnitSpec extends UnitSpec {
 
   "submit" should {
 
-    "redirect to Payment page when valid submit and user type is Business" in pending
-    "redirect to Payment page when valid submit and user type is Keeper" in pending
-    "write cookie when uprn found" in pending
+    "redirect to Payment page when valid submit and user type is Business" in {
+      val request = buildRequest().
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel(keeperConsent = UserType_Business)).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.businessDetailsModel())
+      val result = confirm.submit(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(PaymentPage.address))
+      }
+    }
+
+    "redirect to Payment page when valid submit and user type is Keeper" in {
+      val request = buildRequest().
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel(keeperConsent = UserType_Keeper)).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.businessDetailsModel())
+      val result = confirm.submit(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(PaymentPage.address))
+      }
+    }
+
+    "write StoreBusinessDetails cookie when user type is Business and has not provided a keeperEmail" in {
+      val request = buildRequest(keeperEmail = "").
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel(keeperConsent = UserType_Business)).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.businessDetailsModel())
+      val result = confirm.submit(request)
+      whenReady(result) { r =>
+        val cookies = fetchCookiesFromHeaders(r)
+        cookies.map(_.name) should contain (StoreBusinessDetailsCacheKey)
+      }
+    }
+
+    "write StoreBusinessDetails cookie when user type is Business and has provided a keeperEmail" in {
+      val request = buildRequest().
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel(keeperConsent = UserType_Business)).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.businessDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.keeperEmail())
+      val result = confirm.submit(request)
+      whenReady(result) { r =>
+        val cookies = fetchCookiesFromHeaders(r)
+        cookies.map(_.name) should contain allOf (KeeperEmailCacheKey, StoreBusinessDetailsCacheKey)
+      }
+    }
+
+    "not write cookies when user type is Keeper and has not provided a keeperEmail" in {
+      val request = buildRequest(keeperEmail = "").
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel(keeperConsent = UserType_Keeper)).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.businessDetailsModel())
+      val result = confirm.submit(request)
+      whenReady(result) { r =>
+        val cookies = fetchCookiesFromHeaders(r)
+        cookies.map(_.name) should be (empty)
+      }
+    }
+
+    "write KeeperEmail cookie when user type is Keeper and has provided a keeperEmail" in {
+      val request = buildRequest().
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel(keeperConsent = UserType_Keeper)).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperDetailsModel()).
+        withCookies(CookieFactoryForUnitSpecs.businessDetailsModel())
+      val result = confirm.submit(request)
+      whenReady(result) { r =>
+        val cookies = fetchCookiesFromHeaders(r)
+        cookies.map(_.name) should contain (KeeperEmailCacheKey)
+      }
+    }
   }
 
   private lazy val present = {
@@ -54,4 +123,11 @@ final class ConfirmUnitSpec extends UnitSpec {
     confirm.present(request)
   }
   private val confirm = injector.getInstance(classOf[Confirm])
+
+  private def buildRequest(keeperEmail: String = KeeperEmailValid.get, storeDetailsConsent: Boolean = false) = {
+    FakeRequest().withFormUrlEncodedBody(
+      KeeperEmailId -> keeperEmail,
+      StoreDetailsConsentId -> storeDetailsConsent.toString
+    )
+  }
 }
