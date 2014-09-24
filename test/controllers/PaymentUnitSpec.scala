f2e4d9dbb8e4f15f6b3f99af91a70ad5d7e5b7df
Scott Walker <scott.walker@valtech.co.uk>
2014-09-24 17:19:28 +0100

    US1190: Test coverage for payment/begin

--- test/controllers/PaymentUnitSpec.scala
+++ test/controllers/PaymentUnitSpec.scala
@@ -2,11 +2,59 @@ package controllers
 
 import helpers.vrm_retention.CookieFactoryForUnitSpecs
 import helpers.{UnitSpec, WithApplication}
-import pages.vrm_retention.{PaymentCallbackPage, MockFeedbackPage}
-import play.api.test.FakeRequest
+import pages.vrm_retention.{MicroServiceErrorPage, MockFeedbackPage, PaymentCallbackPage}
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.{FakeHeaders, FakeRequest}
 import play.api.test.Helpers.{LOCATION, OK, contentAsString, _}
+import play.api.test.FakeHeaders
 
 final class PaymentUnitSpec extends UnitSpec {
+
+  "begin" should {
+
+    "redirect to MicroServiceError page when TransactionId cookie does not exist" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel())
+      val result = payment.begin(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(MicroServiceErrorPage.address))
+      }
+    }
+
+    "redirect to MicroServiceError page when VehicleAndKeeperLookupFormModel cookie does not exist" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(CookieFactoryForUnitSpecs.transactionId())
+      val result = payment.begin(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(MicroServiceErrorPage.address))
+      }
+    }
+
+    "redirect to MicroServiceError page when no referer in request" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(CookieFactoryForUnitSpecs.transactionId()).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel())
+
+      val result = payment.begin(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(MicroServiceErrorPage.address))
+      }
+    }
+
+    "redirect to Payment page when required cookies exist" in new WithApplication {
+      val referer = Seq("somewhere-made-up")
+      val refererHeader = (REFERER, referer)
+      val headers = FakeHeaders(data = Seq(refererHeader))
+      val request = FakeRequest(method = "GET", uri = "/", headers = headers, body = AnyContentAsEmpty).
+        withCookies(CookieFactoryForUnitSpecs.transactionId()).
+        withCookies(CookieFactoryForUnitSpecs.vehicleAndKeeperLookupFormModel())
+
+      val result = payment.begin(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(MicroServiceErrorPage.address))
+      }
+    }
+  }
 
   "exit" should {
 
