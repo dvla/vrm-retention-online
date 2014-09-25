2a672eba70fda5ef14a6486f020d186f1c12b6b8
Scott Walker <scott.walker@valtech.co.uk>
2014-09-25 13:05:29 +0100

    US1190: trying without iframe

--- app/controllers/Payment.scala
+++ app/controllers/Payment.scala
@@ -115,10 +115,11 @@ final class Payment @Inject()(vrmRetentionRetainService: VRMRetentionRetainServi
         paymentSolveService.invoke(paymentSolveBeginRequest, trackingId).map {
           response =>
             if ((response.response == VALIDATED_RESPONSE) && (response.status == CARD_DETAILS_STATUS)) {
-              Ok(views.html.vrm_retention.payment(paymentRedirectUrl = response.redirectUrl.get)) // TODO need sad path for when redirectUrl is None
-                .withCookie(PaymentTransactionReferenceCacheKey, response.trxRef.get)
-                .withCookie(CsrfPreventionAction.TokenName, token.value) // TODO delete this cookie in the payment callback.
-                .withCookie(REFERER, routes.Payment.begin().url) // TODO delete this cookie in the payment callback.
+//              Ok(views.html.vrm_retention.payment(paymentRedirectUrl = response.redirectUrl.get)) // TODO need sad path for when redirectUrl is None
+//                .withCookie(PaymentTransactionReferenceCacheKey, response.trxRef.get)
+//                .withCookie(CsrfPreventionAction.TokenName, token.value) // TODO delete this cookie in the payment callback.
+//                .withCookie(REFERER, routes.Payment.begin().url) // TODO delete this cookie in the payment callback.
+              Redirect(response.redirectUrl.get)
             } else {
               Logger.error("The begin web request to Solve was not validated.")
               paymentBeginFailure
