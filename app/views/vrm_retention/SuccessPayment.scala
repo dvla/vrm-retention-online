277d2a3d45db4e3c5c1fe7219814578eb331f4fd
Scott Walker <scott.walker@valtech.co.uk>
2014-10-07 15:27:12 +0100

    US1326: split the confirm page

---
+++ app/views/vrm_retention/SuccessPayment.scala
@@ -0,0 +1,7 @@
+package views.vrm_retention
+
+object SuccessPayment {
+
+  final val NextId = "next"
+  final val PreviewEmailId = "previewEmail"
+}
---
+++ test/views/vrm_retention/SuccessPaymentUiSpec.scala
@@ -0,0 +1,49 @@
+package views.vrm_retention
+
+import helpers.UiSpec
+import helpers.tags.UiTag
+import helpers.vrm_retention.CookieFactoryForUISpecs
+import helpers.webbrowser.TestHarness
+import org.openqa.selenium.WebDriver
+import pages.vrm_retention.SuccessPaymentPage.next
+import pages.vrm_retention.{BeforeYouStartPage, SuccessPage, SuccessPaymentPage}
+
+final class SuccessPaymentUiSpec extends UiSpec with TestHarness {
+
+  "go to page" should {
+
+    "display the page" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to SuccessPaymentPage
+
+      page.url should equal(SuccessPaymentPage.url)
+    }
+  }
+
+  "next" should {
+
+    "redirect to Success page" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to SuccessPaymentPage
+      click on next
+
+      page.url should equal(SuccessPage.url)
+    }
+  }
+
+  private def cacheSetup()(implicit webDriver: WebDriver) =
+    CookieFactoryForUISpecs.
+      vehicleAndKeeperLookupFormModel().
+      setupBusinessDetails().
+      businessChooseYourAddress().
+      vehicleAndKeeperDetailsModel().
+      enterAddressManually().
+      businessDetails().
+      eligibilityModel().
+      keeperEmail().
+      retainModel().
+      transactionId().
+      paymentTransactionReference()
+}
