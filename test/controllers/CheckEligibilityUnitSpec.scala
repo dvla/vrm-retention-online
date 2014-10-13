e4a9244b6b92e7264ab3b4818732c6527682aaac
Scott Walker <scott.walker@valtech.co.uk>
2014-10-13 15:37:29 +0100

    Coverage: tests for Elig web service call fails, fixed problem in other tests

--- test/controllers/CheckEligibilityUnitSpec.scala
+++ test/controllers/CheckEligibilityUnitSpec.scala
@@ -1,8 +1,9 @@
 package controllers
 
+import composition.eligibility.EligibilityWebServiceCallFails
 import helpers.vrm_retention.CookieFactoryForUnitSpecs._
 import helpers.{UnitSpec, WithApplication}
-import pages.vrm_retention.ErrorPage
+import pages.vrm_retention.{ErrorPage, MicroServiceErrorPage}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
@@ -19,7 +20,7 @@ final class CheckEligibilityUnitSpec extends UnitSpec {
 
     "redirect to error page when VehicleAndKeeperDetailsModel cookie does not exist" in new WithApplication {
       val request = FakeRequest().
-        withCookies(vehicleAndKeeperDetailsModel())
+        withCookies(vehicleAndKeeperLookupFormModel())
       val result = checkEligibility.present(request)
       whenReady(result) { r =>
         r.header.headers.get(LOCATION).get.startsWith(ErrorPage.address)
@@ -29,7 +30,7 @@ final class CheckEligibilityUnitSpec extends UnitSpec {
     "redirect to error page when StoreBusinessDetailsCacheKey cookie does not exist" in new WithApplication {
       val request = FakeRequest().
         withCookies(
-          vehicleAndKeeperDetailsModel(),
+          vehicleAndKeeperLookupFormModel(),
           vehicleAndKeeperDetailsModel()
         )
       val result = checkEligibility.present(request)
@@ -41,7 +42,7 @@ final class CheckEligibilityUnitSpec extends UnitSpec {
     "redirect to error page when TransactionIdCacheKey cookie does not exist" in new WithApplication {
       val request = FakeRequest().
         withCookies(
-          vehicleAndKeeperDetailsModel(),
+          vehicleAndKeeperLookupFormModel(),
           vehicleAndKeeperDetailsModel(),
           storeBusinessDetailsConsent()
         )
@@ -50,7 +51,28 @@ final class CheckEligibilityUnitSpec extends UnitSpec {
         r.header.headers.get(LOCATION).get.startsWith(ErrorPage.address)
       }
     }
+
+    "redirect to micro-service error page when web service call fails" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(
+          vehicleAndKeeperLookupFormModel(),
+          vehicleAndKeeperDetailsModel(),
+          storeBusinessDetailsConsent(),
+          transactionId()
+        )
+      val result = checkEligibilityCallFails().present(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION) should equal(Some(MicroServiceErrorPage.address))
+      }
+    }
   }
 
   private lazy val checkEligibility = testInjector().getInstance(classOf[CheckEligibility])
+
+  private def checkEligibilityCallFails() = {
+    testInjector(
+      new EligibilityWebServiceCallFails()
+    ).
+      getInstance(classOf[CheckEligibility])
+  }
 }
