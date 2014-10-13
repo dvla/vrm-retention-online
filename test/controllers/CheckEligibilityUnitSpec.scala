4acd0c4e49a3d4104b8c87d8b094b19caff0a8b0
Scott Walker <scott.walker@valtech.co.uk>
2014-10-13 15:25:02 +0100

    Code coverage: CheckEligibility controller

--- test/controllers/CheckEligibilityUnitSpec.scala
+++ test/controllers/CheckEligibilityUnitSpec.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import helpers.vrm_retention.CookieFactoryForUnitSpecs._
 import helpers.{UnitSpec, WithApplication}
 import pages.vrm_retention.ErrorPage
 import play.api.test.FakeRequest
@@ -11,6 +12,40 @@ final class CheckEligibilityUnitSpec extends UnitSpec {
 
     "redirect to error page when VehicleAndKeeperLookupFormModel cookie does not exist" in new WithApplication {
       val result = checkEligibility.present(FakeRequest())
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION).get.startsWith(ErrorPage.address)
+      }
+    }
+
+    "redirect to error page when VehicleAndKeeperDetailsModel cookie does not exist" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(vehicleAndKeeperDetailsModel())
+      val result = checkEligibility.present(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION).get.startsWith(ErrorPage.address)
+      }
+    }
+
+    "redirect to error page when StoreBusinessDetailsCacheKey cookie does not exist" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(
+          vehicleAndKeeperDetailsModel(),
+          vehicleAndKeeperDetailsModel()
+        )
+      val result = checkEligibility.present(request)
+      whenReady(result) { r =>
+        r.header.headers.get(LOCATION).get.startsWith(ErrorPage.address)
+      }
+    }
+
+    "redirect to error page when TransactionIdCacheKey cookie does not exist" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(
+          vehicleAndKeeperDetailsModel(),
+          vehicleAndKeeperDetailsModel(),
+          storeBusinessDetailsConsent()
+        )
+      val result = checkEligibility.present(request)
       whenReady(result) { r =>
         r.header.headers.get(LOCATION).get.startsWith(ErrorPage.address)
       }
