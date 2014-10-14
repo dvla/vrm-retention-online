7b6c7664addf7690ea9f5222f98ae50ed2808aa2
Scott Walker <scott.walker@valtech.co.uk>
2014-10-14 08:46:47 +0100

    Test Audit service stubs explicitly doing nothing

--- test/composition/TestAuditService.scala
+++ test/composition/TestAuditService.scala
@@ -1,13 +1,17 @@
 package composition
 
-import audit.AuditService
+import audit.{AuditMessage, AuditService}
 import com.tzavellas.sse.guice.ScalaModule
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.mockito.internal.stubbing.answers.DoesNothing
 import org.scalatest.mock.MockitoSugar
 
 class TestAuditService extends ScalaModule with MockitoSugar {
 
   def configure() = {
     val dateService = mock[AuditService]
+    when(dateService.send(any[AuditMessage])).thenAnswer(new DoesNothing)
     bind[AuditService].toInstance(dateService)
   }
 }
