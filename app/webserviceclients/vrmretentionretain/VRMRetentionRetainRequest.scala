a901c6bc8a15af11f2514bdf372b929836d844a8
Scott Walker <scott.walker@valtech.co.uk>
2014-10-22 09:52:32 +0100

    Fix epoch for the Retain request, added tests

--- app/webserviceclients/vrmretentionretain/VRMRetentionRetainRequest.scala
+++ app/webserviceclients/vrmretentionretain/VRMRetentionRetainRequest.scala
@@ -1,11 +1,20 @@
 package webserviceclients.vrmretentionretain
 
-import play.api.libs.json.Json
+import org.joda.time.format.ISODateTimeFormat
+import play.api.libs.json.{JsString, JsValue, Writes, Json}
 import org.joda.time.DateTime
 
 case class VRMRetentionRetainRequest(currentVRM: String, transactionTimestamp: DateTime)
 
 object VRMRetentionRetainRequest {
+
+  // Transform a DateTime from the default milliseconds to an ISO formatted string, e.g. 2014-03-04T00:00:00.000Z
+  implicit val jodaISODateWrites: Writes[DateTime] = new Writes[DateTime] {
+    override def writes(dateTime: DateTime): JsValue = {
+      val formatter = ISODateTimeFormat.dateTime
+      JsString(formatter.print(dateTime))
+    }
+  }
 
   implicit val JsonFormat = Json.format[VRMRetentionRetainRequest]
 }
