22506c03a8ae6c5559cb51b4f86e60f9dc46b638
Scott Walker <scott.walker@valtech.co.uk>
2014-10-27 11:20:54 +0000

    HTML ids with hypthens for readability

--- app/views/vrm_retention/BusinessChooseYourAddress.scala
+++ app/views/vrm_retention/BusinessChooseYourAddress.scala
@@ -3,8 +3,8 @@ package views.vrm_retention
 object BusinessChooseYourAddress {
 
   final val BusinessChooseYourAddressCacheKey = "businessChooseYourAddress"
-  final val AddressSelectId = "vrm_retention_businessChooseYourAddress_addressSelect"
-  final val EnterAddressManuallyButtonId = "enterAddressManuallyButton"
+  final val AddressSelectId = "address-select"
+  final val EnterAddressManuallyButtonId = "enter-address-manually"
   final val SelectId = "select"
   final val ExitId = "exit"
 }
