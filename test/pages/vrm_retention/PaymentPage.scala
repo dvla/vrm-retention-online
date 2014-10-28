7b4f64af64d8c8d25eb191dcf8410932acb852db
Scott Walker <scott.walker@valtech.co.uk>
2014-10-28 15:30:38 +0000

    Payment UI specs uncommented

--- test/pages/vrm_retention/PaymentPage.scala
+++ test/pages/vrm_retention/PaymentPage.scala
@@ -12,17 +12,5 @@ object PaymentPage extends Page with WebBrowserDSL {
   def url = WebDriverFactory.testUrl + address.substring(1)
   final override val title: String = "Payment details"
 
-  def payNow(implicit driver: WebDriver): Element = find(id(CancelId)).get
-
-  def exit(implicit driver: WebDriver): Element = find(id(ExitId)).get
-
-  def happyPath(implicit driver: WebDriver) = {
-    go to PaymentPage
-    click on payNow
-  }
-
-  def exitPath(implicit driver: WebDriver) = {
-    go to PaymentPage
-    click on exit
-  }
+  def cancel(implicit driver: WebDriver): Element = find(id(CancelId)).get
 }
--- test/views/vrm_retention/PaymentIntegrationSpec.scala
+++ test/views/vrm_retention/PaymentIntegrationSpec.scala
@@ -5,113 +5,97 @@ import helpers.tags.UiTag
 import helpers.webbrowser.TestHarness
 import org.openqa.selenium.{WebDriver, WebElement, By}
 import pages.vrm_retention._
-import pages.vrm_retention.PaymentPage.{exitPath, happyPath}
 import helpers.vrm_retention.CookieFactoryForUISpecs
 import views.vrm_retention.RelatedCacheKeys.{BusinessDetailsSet, RetainSet}
 
 final class PaymentIntegrationSpec extends UiSpec with TestHarness {
 
-//  "go to page" should {
-// TODO restore when payment iframe is back
-//    "display the page" taggedAs UiTag in new WebBrowser {
-//      go to BeforeYouStartPage
-//
-//      cacheSetup()
-//
-//      go to PaymentPage
-//
-//      page.url should equal(PaymentPage.url)
-//    }
+  "go to page" should {
+    "display the page" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
 
-    // TODO not sure what this test is doing going ot the Vehicle Lookup page
-//    "contain the hidden csrfToken field" taggedAs UiTag in new WebBrowser {
-//      go to VehicleLookupPage
-//      val csrf: WebElement = webDriver.findElement(By.name(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName))
-//      csrf.getAttribute("type") should equal("hidden")
-//      csrf.getAttribute("name") should equal(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName)
-//      csrf.getAttribute("value").size > 0 should equal(true)
-//    }
-//  }
+      go to PaymentPage
 
-// TODO restore when payment iframe is back
-//  "pay now button" should {
-//
-//    "redirect to summary page when pay now link is clicked" taggedAs UiTag in new WebBrowser {
-//      go to BeforeYouStartPage
-//
-//      cacheSetup()
-//
-//      happyPath
-//
-//      page.url should equal(SummaryPage.url)
-//    }
-//  }
+      page.url should equal(PaymentPage.url)
+    }
 
-//  "exit" should {
-// TODO restore when payment iframe is back
-//    "display feedback page when exit link is clicked" taggedAs UiTag in new WebBrowser {
-//      go to BeforeYouStartPage
-//
-//      cacheSetup()
-//
-//      exitPath
-//
-//      page.url should equal(MockFeedbackPage.url)
-//    }
+    "contain the hidden csrfToken field" taggedAs UiTag in new WebBrowser {
+      go to VehicleLookupPage
+      val csrf: WebElement = webDriver.findElement(By.name(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName))
+      csrf.getAttribute("type") should equal("hidden")
+      csrf.getAttribute("name") should equal(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName)
+      csrf.getAttribute("value").size > 0 should equal(true)
+    }
+  }
 
-//    "remove RetainSet cookies when storeBusinessDetailsConsent cookie does not exist" taggedAs UiTag in new WebBrowser {
-//      go to BeforeYouStartPage
-//
-//      cacheSetup()
-//
-//      exitPath
-//
-//      // Verify the cookies identified by the full set of cache keys have been removed
-//      RetainSet.foreach(cacheKey => {
-//        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
-//      })
-//    }
-// TODO restore when payment iframe is back
-//    "remove RetainSet and BusinessDetailsSet cookies when storeBusinessDetailsConsent cookie is false" taggedAs UiTag in new WebBrowser {
-//      go to BeforeYouStartPage
-//
-//      cacheSetup().
-//        businessChooseYourAddress().
-//        setupBusinessDetails().
-//        storeBusinessDetailsConsent(consent = "false")
-//
-//      exitPath
-//
-//      // Verify the cookies identified by the full set of cache keys have been removed
-//      BusinessDetailsSet.foreach(cacheKey => {
-//        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
-//      })
-//
-//      RetainSet.foreach(cacheKey => {
-//        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
-//      })
-//    }
+//  Cannot test without mocking up the html of the Solve payment iframe
+//  "pay now button" should
 
-//    "remove RetainSet cookies when storeBusinessDetailsConsent cookie contains true" taggedAs UiTag in new WebBrowser {
-//      go to BeforeYouStartPage
-//
-//      cacheSetup().
-//        businessChooseYourAddress().
-//        setupBusinessDetails().
-//        storeBusinessDetailsConsent(consent = "true")
-//
-//      exitPath
-//
-//      // Verify the cookies identified by the full set of cache keys have been removed
-//      BusinessDetailsSet.foreach(cacheKey => {
-//        webDriver.manage().getCookieNamed(cacheKey) should not equal null // Verify not removed in this case!
-//      })
-//
-//      RetainSet.foreach(cacheKey => {
-//        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
-//      })
-//    }
-//  }
+  "cancel" should {
+    "redirect to mock feedback page" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to PaymentPage
+
+      click on PaymentPage.cancel
+
+      page.url should equal(MockFeedbackPage.url)
+    }
+
+    "remove RetainSet cookies when storeBusinessDetailsConsent cookie does not exist" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to PaymentPage
+
+      click on PaymentPage.cancel
+
+      // Verify the cookies identified by the full set of cache keys have been removed
+      RetainSet.foreach(cacheKey => {
+        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
+      })
+    }
+
+    "remove RetainSet and BusinessDetailsSet cookies when storeBusinessDetailsConsent cookie is false" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup().
+        businessChooseYourAddress().
+        setupBusinessDetails().
+        storeBusinessDetailsConsent(consent = "false")
+      go to PaymentPage
+
+      click on PaymentPage.cancel
+
+      // Verify the cookies identified by the full set of cache keys have been removed
+      BusinessDetailsSet.foreach(cacheKey => {
+        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
+      })
+
+      RetainSet.foreach(cacheKey => {
+        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
+      })
+    }
+
+    "remove RetainSet cookies when storeBusinessDetailsConsent cookie contains true" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup().
+        businessChooseYourAddress().
+        setupBusinessDetails().
+        storeBusinessDetailsConsent(consent = "true")
+      go to PaymentPage
+
+      click on PaymentPage.cancel
+
+      // Verify the cookies identified by the full set of cache keys have been removed
+      BusinessDetailsSet.foreach(cacheKey => {
+        webDriver.manage().getCookieNamed(cacheKey) should not equal null // Verify not removed in this case!
+      })
+
+      RetainSet.foreach(cacheKey => {
+        webDriver.manage().getCookieNamed(cacheKey) should equal(null)
+      })
+    }
+  }
 
   private def cacheSetup()(implicit webDriver: WebDriver) =
     CookieFactoryForUISpecs.
@@ -120,6 +104,6 @@ final class PaymentIntegrationSpec extends UiSpec with TestHarness {
       businessDetails().
       eligibilityModel().
       keeperEmail().
-      retainModel().
-      transactionId()
+      transactionId().
+      paymentTransNo()
 }
