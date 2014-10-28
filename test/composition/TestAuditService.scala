553cea3c70095c97a5d5b1c21618844f267be2e9
Geraint Jones <geraint.jones@valtech.co.uk>
2014-10-28 08:57:44 +0000

    pass a mock audit service to TestAuditService

--- test/composition/TestAuditService.scala
+++ test/composition/TestAuditService.scala
@@ -3,14 +3,13 @@ package composition
 import audit.{Message, AuditService}
 import com.tzavellas.sse.guice.ScalaModule
 import org.mockito.Matchers.any
-import org.mockito.Mockito.when
+import org.mockito.Mockito.{mock, when}
 import org.mockito.internal.stubbing.answers.DoesNothing
 import org.scalatest.mock.MockitoSugar
 
-class TestAuditService extends ScalaModule with MockitoSugar {
+class TestAuditService(auditService: AuditService = mock(classOf[AuditService])) extends ScalaModule with MockitoSugar {
 
   def configure() = {
-    val auditService = mock[AuditService]
     when(auditService.send(any[Message])).thenAnswer(new DoesNothing)
     bind[AuditService].toInstance(auditService)
   }
