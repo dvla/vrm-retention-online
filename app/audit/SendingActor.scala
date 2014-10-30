fe709ec67131fa3d246a2f44ff9b160669f8ea52
Scott Walker <scott.walker@valtech.co.uk>
2014-10-30 16:14:53 +0000

    rabbit-mq message stubbed until we can make it sent IEP json

--- app/audit/SendingActor.scala
+++ app/audit/SendingActor.scala
@@ -2,13 +2,15 @@ package audit
 
 import akka.actor.Actor
 import com.rabbitmq.client.Channel
+import play.api.libs.json.Json
 import uk.gov.dvla.auditing.Message
 
 class SendingActor(channel: Channel, queue: String) extends Actor {
 
   def receive = {
     case auditMessage: Message =>
-      val message = auditMessage.toJson.getBytes
+//      val message = auditMessage.toJson.getBytes
+      val message = Json.stringify(Json.toJson(Json.obj(("stub-key", "hello-world")))).getBytes
       channel.basicPublish("", queue, null, message)
     case _ =>
   }
