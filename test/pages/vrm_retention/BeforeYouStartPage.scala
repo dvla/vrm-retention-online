880c0079f31be23522d067700c7deb31a97da8ed
Geraint Jones <geraint.jones@valtech.co.uk>
2014-11-13 13:28:58 +0000

    fix to url compare in before you start test

--- test/pages/vrm_retention/BeforeYouStartPage.scala
+++ test/pages/vrm_retention/BeforeYouStartPage.scala
@@ -9,7 +9,7 @@ object BeforeYouStartPage extends Page with WebBrowserDSL {
 
   def address = s"$applicationContext/before-you-start"
   def url = WebDriverFactory.testUrl + address.substring(1)
-  final override val title: String = "Retain a personalised registration"
+  final override val title: String = "Take a Registration Number off a Vehicle"
   final val titleCy: String = "Cael gwared cerbyd i mewn i'r fasnach foduron"
 
   def startNow(implicit driver: WebDriver): Element = find(id(NextId)).get
