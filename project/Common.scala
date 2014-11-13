c9c6a6bbe9e7de0ec200b2f78c68259d5921d50f
jamesh <james.heggs@valtech.co.uk>
2014-11-13 08:43:37 +0000

    Revert version to non-release snapshot

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt.Keys.version
 import sbt._
 
 object Common {
-  val versionString = "1.3"
+  val versionString = "1.0-SNAPSHOT"
   val scalaVersionString = "2.10.3"
   val organisationString = "dvla"
   val organisationNameString = "Driver & Vehicle Licensing Agency"
