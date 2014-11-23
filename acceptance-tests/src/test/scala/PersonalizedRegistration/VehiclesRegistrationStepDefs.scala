63ecb3567b924f00105173155f514bfd761faa3e
Tanvi Nanda <tanvinanda@Tanvis-MacBook-Pro.local>
2014-11-23 01:55:33 +0000

    Cucumber project fix

--- acceptance-tests/src/test/scala/PersonalizedRegistration/VehiclesRegistrationStepDefs.scala
+++ acceptance-tests/src/test/scala/PersonalizedRegistration/VehiclesRegistrationStepDefs.scala
@@ -33,18 +33,20 @@ final class VehiclesRegistrationStepDefs(webBrowserDriver: WebBrowserDriver) ext
     User.ConfirmDetails()
   }
 
+  //Scenario 2 -
   @When("^I enter invalid data in the \"(.*?)\" \"(.*?)\" and \"(.*?)\" fields$")
-  def i_enter_invalid_data_in_the_and_fields(arg0: String, arg1: String, arg2: String) {
-
+  def i_enter_invalid_data_in_the_and_fields(RegistrationNumber: String, DocRef: String, PostCode: String) {
+    User.EnterRegistrationNumberDocRefNumberAndPostcode(RegistrationNumber, DocRef, PostCode)
+    User.IndicateKeeperIsNotActing()
   }
 
   @Then("^the error messages for invalid data in the Vehicle Registration Number, Doc Ref ID and Postcode fields are displayed$")
   def the_error_messages_for_invalid_data_in_the_Vehicle_Registration_Number_Doc_Ref_ID_and_Postcode_fields_are_displayed() {
-
+    User.GetsInvalidMessages()
   }
 
   @When("^I enter data in the \"(.*?)\" \"(.*?)\" and \"(.*?)\" that does not match a valid vehicle record three times in a row$")
-  def i_enter_data_in_the_and_that_does_not_match_a_valid_vehicle_record_three_times_in_a_row(arg0: String, arg1: String, arg2: String) {
+  def i_enter_data_in_the_and_that_does_not_match_a_valid_vehicle_record_three_times_in_a_row(RegistrationNumber: String, DocRef: String, PostCode: String) {
 
   }
 
