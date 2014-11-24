968d550a8ad8d9a6c7f8755a744445592719e8ae
Scott Walker <scott.walker@valtech.co.uk>
2014-11-24 10:49:45 +0000

    Updated sandbox micro-services to release versions

--- project/Sandbox.scala
+++ project/Sandbox.scala
@@ -23,7 +23,7 @@ object Sandbox extends Plugin {
   final val VersionVehicleAndKeeperLookup = "0.2"
   final val VersionVrmRetentionEligibility = "0.4"
   final val VersionVrmRetentionRetain = "0.3"
-  final val VersionPaymentSolve= "0.3-SNAPSHOT"
+  final val VersionPaymentSolve= "0.3"
   final val VersionLegacyStubs = "1.0-SNAPSHOT"
   final val VersionJetty = "9.2.1.v20140609"
   final val VersionSpringWeb = "3.0.7.RELEASE"
