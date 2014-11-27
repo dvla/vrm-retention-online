829accfd62c9a0cd509dd25ccd4b7891c0d87b6f
Geraint Jones <geraint.jones@valtech.co.uk>
2014-11-27 15:11:08 +0000

    fix to tests due to addition of transaction timestamp to v&k lookup request

--- app/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsRequest.scala
+++ app/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsRequest.scala
@@ -1,19 +1,31 @@
 package webserviceclients.vehicleandkeeperlookup
 
 import models.VehicleAndKeeperLookupFormModel
-import play.api.libs.json.Json
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+import play.api.libs.json.{JsString, JsValue, Writes, Json}
 
 final case class VehicleAndKeeperDetailsRequest(referenceNumber: String,
-                                                registrationNumber: String)
+                                                registrationNumber: String,
+                                                transactionTimestamp: DateTime)
 
 object VehicleAndKeeperDetailsRequest {
 
+  // Handles this type of formatted string 2014-03-04T00:00:00.000Z
+  implicit val jodaISODateWrites: Writes[DateTime] = new Writes[DateTime] {
+    override def writes(dateTime: DateTime): JsValue = {
+      val formatter = ISODateTimeFormat.dateTime
+      JsString(formatter.print(dateTime))
+    }
+  }
+
   implicit val JsonFormat = Json.format[VehicleAndKeeperDetailsRequest]
 
-  def from(vehicleAndKeeperLookupFormModel: VehicleAndKeeperLookupFormModel): VehicleAndKeeperDetailsRequest = {
+  def from(vehicleAndKeeperLookupFormModel: VehicleAndKeeperLookupFormModel, transactionTimestamp: DateTime): VehicleAndKeeperDetailsRequest = {
     VehicleAndKeeperDetailsRequest(
       referenceNumber = vehicleAndKeeperLookupFormModel.referenceNumber,
-      registrationNumber = vehicleAndKeeperLookupFormModel.registrationNumber
+      registrationNumber = vehicleAndKeeperLookupFormModel.registrationNumber,
+      transactionTimestamp = transactionTimestamp
     )
   }
 }
