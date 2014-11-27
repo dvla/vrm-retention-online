db7e781a5a313ffe6214f4e4b0fbde6b10b74147
Scott Walker <scott.walker@valtech.co.uk>
2014-11-27 12:21:53 +0000

    US1580: moving steps into files for each page

--- acceptance-tests/src/test/scala/PersonalizedRegistration/VehiclesRegistrationStepDefs.scala
+++ acceptance-tests/src/test/scala/PersonalizedRegistration/VehiclesRegistrationStepDefs.scala
@@ -1,6 +1,6 @@
 package PersonalizedRegistration
 
-import common.CommonStepDefs
+import common.{BeforeYouStartPageSteps, VehicleLookupPageSteps, CommonStepDefs}
 import cucumber.api.java.en.{Given, Then, When}
 import cucumber.api.scala.{EN, ScalaDsl}
 import helpers.webbrowser.{WebBrowserDSL, WebBrowserDriver}
@@ -8,64 +8,68 @@ import org.scalatest.Matchers
 
 final class VehiclesRegistrationStepDefs(implicit webDriver: WebBrowserDriver) extends ScalaDsl with EN with WebBrowserDSL with Matchers {
 
-  lazy val user = new CommonStepDefs
+  lazy val common = new CommonStepDefs
+  lazy val beforeYouStart = new BeforeYouStartPageSteps
+  lazy val vehicleLookup = new VehicleLookupPageSteps
 
   @Given("^that I have started the PR Retention Service$")
   def `that I have started the PR Retention Service`() {
-    user.`start the PR service`()
+    common.`start the PR service`()
   }
 
   @When( """^I enter data in the "(.*?)", "(.*?)" and "(.*?)" for a vehicle that is eligible for retention$""")
-  def `I enter data in the <VehicleRegistrationNumber>, <DocRefID> and <Postcode> for a vehicle that is eligible for retention`(RegistrationNumber: String, DocRef: String, PostCode: String) {
-    user.enterRegistrationNumberDocRefNumberAndPostcode(RegistrationNumber, DocRef, PostCode)
-  }
+  def `I enter data in the <VehicleRegistrationNumber>, <DocRefID> and <Postcode> for a vehicle that is eligible for retention`(registrationNumber: String, docRefNumber: String, postcode: String) =
+    vehicleLookup.enter(registrationNumber, docRefNumber, postcode)
 
   @When("^I indicate that the keeper is acting$")
   def `I indicate that the keeper is acting`() {
-    user.indicateKeeperIsActing()
+    vehicleLookup.`keeper is acting`()
+    vehicleLookup.`find vehicle`()
   }
 
   @Then("^the confirm keeper details page is displayed$")
   def `the confirm keeper details page is displayed`() {
-    user.confirmDetails()
+    common.confirmDetails()
   }
 
   //Scenario 2 -
   @When( """^I enter invalid data in the "(.*?)", "(.*?)" and "(.*?)" fields$""")
-  def `I enter invalid data in the <VehicleRegistrationNumber> <DocRefID> and <Postcode> fields`(RegistrationNumber: String, DocRef: String, PostCode: String) {
-    user.enterRegistrationNumberDocRefNumberAndPostcode(RegistrationNumber, DocRef, PostCode)
-    user.indicateKeeperIsNotActing()
+  def `I enter invalid data in the <VehicleRegistrationNumber> <DocRefID> and <Postcode> fields`(registrationNumber: String, docRefNumber: String, postcode: String) {
+    vehicleLookup.enter(registrationNumber, docRefNumber, postcode)
+    vehicleLookup.`keeper is not acting`()
+    vehicleLookup.`find vehicle`()
   }
 
   @Then("^the error messages for invalid data in the Vehicle Registration Number, Doc Ref ID and Postcode fields are displayed$")
   def `the error messages for invalid data in the Vehicle Registration Number, Doc Ref ID and Postcode fields are displayed`() {
-    user.hasInvalidMessages()
+    vehicleLookup.`has error messages`
   }
 
   @When( """^I enter data in the "(.*?)", "(.*?)" and "(.*?)" that does not match a valid vehicle record three times in a row$""")
   def `I enter data in the <VehicleRegistrationNumber>, <DocRefID> and <Postcode> that does not match a valid vehicle record three times in a row`(registrationNumber: String, docRef: String, postcode: String) {
-    user.vehicleLookupDoesNotMatchRecord(registrationNumber, docRef, postcode) // 1st
-    user.goToVehicleLookupPage()
+    common.vehicleLookupDoesNotMatchRecord(registrationNumber, docRef, postcode) // 1st
+    common.goToVehicleLookupPage()
 
-    user.vehicleLookupDoesNotMatchRecord(registrationNumber, docRef, postcode) // 2nd
-    user.goToVehicleLookupPage()
+    common.vehicleLookupDoesNotMatchRecord(registrationNumber, docRef, postcode) // 2nd
+    common.goToVehicleLookupPage()
 
-    user.vehicleLookupDoesNotMatchRecord(registrationNumber, docRef, postcode) // Locked
+    common.vehicleLookupDoesNotMatchRecord(registrationNumber, docRef, postcode) // Locked
   }
 
   @When( """^I enter data in the "(.*?)", "(.*?)" and "(.*?)" that does not match a valid vehicle record$""")
-  def `I enter data in the <Vehicle-Registration-Number>, <Doc-Ref-ID> and <Postcode> that does not match a valid vehicle record`(registrationNumber: String, docRef: String, postcode: String) = {
-    user.enterRegistrationNumberDocRefNumberAndPostcode(registrationNumber, docRef, postcode)
-    user.indicateKeeperIsActing()
+  def `I enter data in the <Vehicle-Registration-Number>, <Doc-Ref-ID> and <Postcode> that does not match a valid vehicle record`(registrationNumber: String, docRefNumber: String, postcode: String) = {
+    vehicleLookup.enter(registrationNumber, docRefNumber, postcode)
+    vehicleLookup.`keeper is acting`()
+    vehicleLookup.`find vehicle`()
   }
 
   @Then( """^the vehicle not found page is displayed$""")
   def `the vehicle not found page is displayed`() = {
-    user.isVehicleNotFoundPage()
+    common.isVehicleNotFoundPage()
   }
 
   @Then("^the brute force lock out page is displayed$")
   def `the brute force lock out page is displayed`() {
-    user.isVrmLockedPage()
+    common.isVrmLockedPage()
   }
 }
--- acceptance-tests/src/test/scala/common/commonStepDefs.scala
+++ acceptance-tests/src/test/scala/common/commonStepDefs.scala
@@ -17,32 +17,9 @@ class CommonStepDefs(implicit webDriver: WebBrowserDriver) extends ScalaDsl with
     vehicleLookup.`is displayed`()
   }
 
-  def enterRegistrationNumberDocRefNumberAndPostcode(RegistrationNumber: String, DocRefNumber: String, Postcode: String) = {
-    VehicleLookupPage.vehicleRegistrationNumber enter RegistrationNumber
-    VehicleLookupPage.documentReferenceNumber enter DocRefNumber
-    VehicleLookupPage.keeperPostcode enter Postcode
-  }
-
-  def indicateKeeperIsActing() = {
-    click on VehicleLookupPage.currentKeeperYes
-    click on VehicleLookupPage.findVehicleDetails
-  }
-
-  def indicateKeeperIsNotActing() = {
-    click on VehicleLookupPage.currentKeeperNo
-    click on VehicleLookupPage.findVehicleDetails
-  }
-
-  def hasInvalidMessages() = {
-    page.source contains "Vehicle registration number - Must be valid format\nDocument reference number - Document reference number must be an 11-digit number"
-  }
-
   def confirmDetails() = {
     page.title should equal(ConfirmPage.title)
     click on ConfirmPage.confirm
-  }
-
-  def makesAPayment() = {
   }
 
   def goToVehicleLookupPageWithNonKeeper(RegistrationNumber: String, DocRefNumber: String, Postcode: String) = {
@@ -66,9 +43,10 @@ class CommonStepDefs(implicit webDriver: WebBrowserDriver) extends ScalaDsl with
     go to VehicleLookupPage
   }
 
-  def vehicleLookupDoesNotMatchRecord(registrationNumber: String, docRef: String, postcode: String) = {
-    enterRegistrationNumberDocRefNumberAndPostcode(registrationNumber, docRef, postcode)
-    indicateKeeperIsNotActing()
+  def vehicleLookupDoesNotMatchRecord(registrationNumber: String, docRefNumber: String, postcode: String) = {
+    vehicleLookup.enter(registrationNumber, docRefNumber, postcode)
+    vehicleLookup.`keeper is not acting`()
+    vehicleLookup.`find vehicle`()
     isVrmLockedPage()
   }
 }
