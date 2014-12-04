bc287fe69cb4ab16f6f111cae715da5304630043
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-12-04 15:31:15 +0000

    US1838 fix a wrong failing test

--- test/views/vrm_retention/VersionIntegrationSpec.scala
+++ test/views/vrm_retention/VersionIntegrationSpec.scala
@@ -9,8 +9,8 @@ class VersionIntegrationSpec extends UiSpec with TestHarness {
   "Version endpoint" should {
     "be declared and should include the build-details.txt from classpath" in new WebBrowser {
       go.to(WebDriverFactory.testUrl + s"$applicationContext/version")
-      val t = fromInputStream(getClass.getResourceAsStream("/build-details.txt")).getLines().toList
-      page.source.lines.toSeq should contain allOf(t.head, t.tail.head, t.tail.tail.toSeq:_*)
+      val t = fromInputStream(getClass.getResourceAsStream("/build-details.txt")).getLines().toSet.toList
+      page.source.lines.toSet.toList should contain allOf(t.head, t.tail.head, t.tail.tail.toSeq:_*)
     }
   }
 }
