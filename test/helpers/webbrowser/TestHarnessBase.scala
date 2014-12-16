37aac13cab36052cb38b1cf0a5568a21fff8b7e3
Scott Walker <scott.walker@valtech.co.uk>
2014-12-16 14:49:26 +0000

    remove commented out code

--- test/helpers/webbrowser/TestHarnessBase.scala
+++ test/helpers/webbrowser/TestHarnessBase.scala
@@ -1,67 +1,24 @@
 package helpers.webbrowser
 
-import composition.TestGlobal
-import play.api.test._
 import org.openqa.selenium.WebDriver
-import org.specs2.mutable.Around
-import org.specs2.specification.Scope
-import play.api.test.TestServer
-import play.api.test.FakeApplication
-import org.specs2.execute.{Result, AsResult}
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.{WebBrowserDSL, WebDriverFactory, TestConfiguration}
-
-// NOTE: Do *not* put any initialisation code in the class below, otherwise delayedInit() gets invoked twice
-// which means around() gets invoked twice and everything is not happy.  Only lazy vals and defs are allowed,
-// no vals or any other code blocks.
-
-//trait TestHarness {
-//  import WebBrowser._
-//  abstract class WebBrowser(val app: FakeApplication = fakeAppWithTestGlobal,
-//                            val port: Int = 9003,
-//                            implicit protected val webDriver: WebDriver = WebDriverFactory.webDriver
-//                             )
-//      extends Around with Scope {
-//
-//
-//
-//    override def around[T: AsResult](t: => T): Result = {
-//      try {
-//        Helpers.running(TestServer(port, app))(AsResult.effectively(t))
-//      } finally {
-//        webDriver.quit()
-//      }
-//    }
-//  }
-//
-//  abstract class HtmlUnitWithJs extends WebBrowser(webDriver = WebDriverFactory.webDriver(
-//    targetBrowser = "htmlUnit",
-//    javascriptEnabled = true)
-//  )
-//
-//  object WebBrowser {
-//    private lazy val fakeAppWithTestGlobal: FakeApplication = FakeApplication(withGlobal = Some(TestGlobal))
-//  }
-//}
-
-
-
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.events.EventFiringWebDriver
 import org.specs2.execute.{AsResult, Result}
 import org.specs2.mutable.Around
 import org.specs2.specification.Scope
-import play.api.GlobalSettings
 import play.api.test.{FakeApplication, TestServer, _}
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.{GlobalCreator, ProgressBar}
+import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.{GlobalCreator, ProgressBar, TestConfiguration, WebDriverFactory}
 
 // NOTE: Do *not* put any initialisation code in the class below, otherwise delayedInit() gets invoked twice
 // which means around() gets invoked twice and everything is not happy.  Only lazy vals and defs are allowed,
 // no vals or any other code blocks.
 
 trait TestHarnessBase extends ProgressBar with GlobalCreator {
+
   import WebBrowser._
+
   abstract class WebBrowser(val app: FakeApplication = fakeAppWithTestGlobal,
                             val port: Int = testPort,
-                            implicit protected val webDriver: WebDriver = WebDriverFactory.webDriver)
+                            implicit protected val webDriver: EventFiringWebDriver = WebDriverFactory.webDriver)
     extends Around with Scope {
 
     override def around[T: AsResult](t: => T): Result =
@@ -72,6 +29,7 @@ trait TestHarnessBase extends ProgressBar with GlobalCreator {
   }
 
   abstract class ProgressBarTrue extends WebBrowser(app = fakeApplicationWithProgressBarTrue)
+
   abstract class ProgressBarFalse extends WebBrowser(app = fakeApplicationWithProgressBarFalse)
 
   abstract class WebBrowserWithJs extends WebBrowser(
@@ -79,7 +37,9 @@ trait TestHarnessBase extends ProgressBar with GlobalCreator {
   )
 
   object WebBrowser {
+
     private lazy val fakeAppWithTestGlobal: FakeApplication = FakeApplication(withGlobal = Some(global))
     private lazy val testPort: Int = TestConfiguration.testPort
   }
+
 }
