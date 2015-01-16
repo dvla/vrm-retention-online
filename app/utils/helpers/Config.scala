a07f39237b636d7e9f7f21c090a5b867c3edda8a
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-01-16 14:15:27 +0000

    US2177: make everything optional

--- app/utils/helpers/Config.scala
+++ app/utils/helpers/Config.scala
@@ -7,70 +7,70 @@ import scala.concurrent.duration.DurationInt
 class Config {
 
   // Payment Service
-  lazy val purchaseAmount: String = getProperty[String]("retention.purchaseAmountInPence")//, "NOT FOUND")
+  lazy val purchaseAmount: String = getOptionalProperty[String]("retention.purchaseAmountInPence").getOrElse("NOT FOUND")
 
-  lazy val isCsrfPreventionEnabled = getProperty[Boolean]("csrf.prevention")//, default = true)
+  lazy val isCsrfPreventionEnabled = getOptionalProperty[Boolean]("csrf.prevention").getOrElse(default = true)
 
   // Micro-service config // TODO take defaults off the timeouts
-  lazy val vehicleAndKeeperLookupMicroServiceBaseUrl: String = getProperty[String]("vehicleAndKeeperLookupMicroServiceUrlBase")//, "NOT FOUND")
-  lazy val vrmRetentionEligibilityMicroServiceUrlBase: String = getProperty[String]("vrmRetentionEligibilityMicroServiceUrlBase")//, "NOT FOUND")
-  lazy val vrmRetentionEligibilityMsRequestTimeout: Int = getProperty[Int]("vrmRetentionEligibility.requesttimeout")//, 30.seconds.toMillis.toInt)
-  lazy val vrmRetentionRetainMicroServiceUrlBase: String = getProperty[String]("vrmRetentionRetainMicroServiceUrlBase")//, "NOT FOUND")
-  lazy val vrmRetentionRetainMsRequestTimeout: Int = getProperty[Int]("vrmRetentionRetain.requesttimeout")//, 30.seconds.toMillis.toInt)
+  lazy val vehicleAndKeeperLookupMicroServiceBaseUrl: String = getOptionalProperty[String]("vehicleAndKeeperLookupMicroServiceUrlBase").getOrElse("NOT FOUND")
+  lazy val vrmRetentionEligibilityMicroServiceUrlBase: String = getOptionalProperty[String]("vrmRetentionEligibilityMicroServiceUrlBase").getOrElse("NOT FOUND")
+  lazy val vrmRetentionEligibilityMsRequestTimeout: Int = getOptionalProperty[Int]("vrmRetentionEligibility.requesttimeout").getOrElse(30.seconds.toMillis.toInt)
+  lazy val vrmRetentionRetainMicroServiceUrlBase: String = getOptionalProperty[String]("vrmRetentionRetainMicroServiceUrlBase").getOrElse("NOT FOUND")
+  lazy val vrmRetentionRetainMsRequestTimeout: Int = getOptionalProperty[Int]("vrmRetentionRetain.requesttimeout").getOrElse(30.seconds.toMillis.toInt)
 
-  lazy val paymentSolveMicroServiceUrlBase: String = getProperty[String]("paymentSolveMicroServiceUrlBase")//, "NOT FOUND")
-  lazy val paymentSolveMsRequestTimeout: Int = getProperty[Int]("paymentSolve.ms.requesttimeout")//, 5.seconds.toMillis.toInt)
+  lazy val paymentSolveMicroServiceUrlBase: String = getOptionalProperty[String]("paymentSolveMicroServiceUrlBase").getOrElse("NOT FOUND")
+  lazy val paymentSolveMsRequestTimeout: Int = getOptionalProperty[Int]("paymentSolve.ms.requesttimeout").getOrElse(5.seconds.toMillis.toInt)
 
   // Ordnance survey config
-  lazy val ordnanceSurveyMicroServiceUrl: String = getProperty[String]("ordnancesurvey.ms.url")//, "NOT FOUND")
-  lazy val ordnanceSurveyRequestTimeout: Int = getProperty[Int]("ordnancesurvey.requesttimeout")//, 5.seconds.toMillis.toInt)
-  lazy val ordnanceSurveyUseUprn: Boolean = getProperty[Boolean]("ordnancesurvey.useUprn")//, default = false)
+  lazy val ordnanceSurveyMicroServiceUrl: String = getOptionalProperty[String]("ordnancesurvey.ms.url").getOrElse("NOT FOUND")
+  lazy val ordnanceSurveyRequestTimeout: Int = getOptionalProperty[Int]("ordnancesurvey.requesttimeout").getOrElse(5.seconds.toMillis.toInt)
+  lazy val ordnanceSurveyUseUprn: Boolean = getOptionalProperty[Boolean]("ordnancesurvey.useUprn").getOrElse(false)
 
-  lazy val vehicleAndKeeperLookupRequestTimeout: Int = getProperty[Int]("vehicleAndKeeperLookup.requesttimeout")//, 30.seconds.toMillis.toInt)
+  lazy val vehicleAndKeeperLookupRequestTimeout: Int = getOptionalProperty[Int]("vehicleAndKeeperLookup.requesttimeout").getOrElse(30.seconds.toMillis.toInt)
 
   // Prototype message in html
-  lazy val isPrototypeBannerVisible: Boolean = getProperty[Boolean]("prototype.disclaimer")//, default = true)
+  lazy val isPrototypeBannerVisible: Boolean = getOptionalProperty[Boolean]("prototype.disclaimer").getOrElse(true)
 
   // Prototype survey URL
-  lazy val prototypeSurveyUrl: String = getProperty[String]("survey.url")//, "")
-  lazy val prototypeSurveyPrepositionInterval: Long = getProperty[Long]("survey.interval")//, 7.days.toMillis)
+  lazy val prototypeSurveyUrl: String = getOptionalProperty[String]("survey.url").getOrElse("")
+  lazy val prototypeSurveyPrepositionInterval: Long = getOptionalProperty[Long]("survey.interval").getOrElse(7.days.toMillis)
 
   // Google analytics
-  lazy val googleAnalyticsTrackingId: Option[String] = getOptionalProperty[String]("googleAnalytics.id.retention")//, "NOT FOUND")
+  lazy val googleAnalyticsTrackingId: Option[String] = getOptionalProperty[String]("googleAnalytics.id.retention")
 
   // Progress step indicator
-  lazy val isProgressBarEnabled: Boolean = getProperty[Boolean]("progressBar.enabled")//, default = true)
+  lazy val isProgressBarEnabled: Boolean = getOptionalProperty[Boolean]("progressBar.enabled").getOrElse(true)
 
   // Audit Service
-  lazy val auditServiceUseRabbit = getProperty[Boolean]("auditService.useRabbit")//, default = false)
+  lazy val auditServiceUseRabbit = getOptionalProperty[Boolean]("auditService.useRabbit").getOrElse(false)
 
   // Rabbit-MQ
-  lazy val rabbitmqHost = getProperty[String]("rabbitmq.host")//, "NOT FOUND")
-  lazy val rabbitmqPort = getProperty[Int]("rabbitmq.port")//, 0)
-  lazy val rabbitmqQueue = getProperty[String]("rabbitmq.queue")//, "NOT FOUND")
-  lazy val rabbitmqUsername = getProperty[String]("rabbitmq.username")//, "NOT FOUND")
-  lazy val rabbitmqPassword = getProperty[String]("rabbitmq.password")//, "NOT FOUND")
-  lazy val rabbitmqVirtualHost = getProperty[String]("rabbitmq.virtualHost")//, "NOT FOUND")
+  lazy val rabbitmqHost = getOptionalProperty[String]("rabbitmq.host").getOrElse("NOT FOUND")
+  lazy val rabbitmqPort = getOptionalProperty[Int]("rabbitmq.port").getOrElse(0)
+  lazy val rabbitmqQueue = getOptionalProperty[String]("rabbitmq.queue").getOrElse("NOT FOUND")
+  lazy val rabbitmqUsername = getOptionalProperty[String]("rabbitmq.username").getOrElse("NOT FOUND")
+  lazy val rabbitmqPassword = getOptionalProperty[String]("rabbitmq.password").getOrElse("NOT FOUND")
+  lazy val rabbitmqVirtualHost = getOptionalProperty[String]("rabbitmq.virtualHost").getOrElse("NOT FOUND")
 
   // Payment Service
-  lazy val renewalFee: String = getProperty[String]("assign.renewalFee")//, "NOT FOUND")
+  lazy val renewalFee: String = getOptionalProperty[String]("assign.renewalFee").getOrElse("NOT FOUND")
 
   // Email Service
-  lazy val emailSmtpHost: String = getProperty[String]("smtp.host")//, "")
-  lazy val emailSmtpPort: Int = getProperty[Int]("smtp.port")//, 25)
-  lazy val emailSmtpSsl: Boolean = getProperty[Boolean]("smtp.ssl")//, default = false)
-  lazy val emailSmtpTls: Boolean = getProperty[Boolean]("smtp.tls")//, default = true)
-  lazy val emailSmtpUser: String = getProperty[String]("smtp.user")//, "")
-  lazy val emailSmtpPassword: String = getProperty[String]("smtp.password")//, "")
-  lazy val emailWhitelist: Option[List[String]] = getStringListProperty("email.whitelist") //getProperty[("email.whitelist", "").split(",")
-  lazy val emailSenderAddress: String = getProperty[String]("email.senderAddress")//, "")
+  lazy val emailSmtpHost: String = getOptionalProperty[String]("smtp.host").getOrElse("")
+  lazy val emailSmtpPort: Int = getOptionalProperty[Int]("smtp.port").getOrElse(25)
+  lazy val emailSmtpSsl: Boolean = getOptionalProperty[Boolean]("smtp.ssl").getOrElse(false)
+  lazy val emailSmtpTls: Boolean = getOptionalProperty[Boolean]("smtp.tls").getOrElse(true)
+  lazy val emailSmtpUser: String = getOptionalProperty[String]("smtp.user").getOrElse("")
+  lazy val emailSmtpPassword: String = getOptionalProperty[String]("smtp.password").getOrElse("")
+  lazy val emailWhitelist: Option[List[String]] = getStringListProperty("email.whitelist") //getOptionalProperty[("email.whitelist", "").split(",")
+  lazy val emailSenderAddress: String = getOptionalProperty[String]("email.senderAddress").getOrElse("")
 
   // Cookie flags
   lazy val secureCookies = getOptionalProperty[Boolean]("secureCookies").getOrElse(true)//, default = true)
-  lazy val cookieMaxAge = getProperty[Int]("application.cookieMaxAge")//, 30.minutes.toSeconds.toInt)
-  lazy val storeBusinessDetailsMaxAge = getProperty[Int]("storeBusinessDetails.cookieMaxAge")//, 7.days.toSeconds.toInt)
+  lazy val cookieMaxAge = getOptionalProperty[Int]("application.cookieMaxAge").getOrElse(30.minutes.toSeconds.toInt)
+  lazy val storeBusinessDetailsMaxAge = getOptionalProperty[Int]("storeBusinessDetails.cookieMaxAge").getOrElse(7.days.toSeconds.toInt)
 
   // Audir microservice
-  val auditMicroServiceUrlBase: String = getProperty[String]("auditMicroServiceUrlBase")//, "NOT FOUND")
-  val auditMsRequestTimeout: Int = getProperty[Int]("audit.requesttimeout")//, 30.seconds.toMillis.toInt)
+  val auditMicroServiceUrlBase: String = getOptionalProperty[String]("auditMicroServiceUrlBase").getOrElse("NOT FOUND")
+  val auditMsRequestTimeout: Int = getOptionalProperty[Int]("audit.requesttimeout").getOrElse(30.seconds.toMillis.toInt)
 }
