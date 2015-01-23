5a5cd280c007904fab6af313d18c81a372d0c6df
Scott Walker <scott.walker@valtech.co.uk>
2015-01-23 11:24:17 +0000

    US2216: parameterised so you pass in the id and the event (e.g. click, change, etc) can be passed in

--- app/assets/javascripts/google-analytics-event-tracking.js
+++ app/assets/javascripts/google-analytics-event-tracking.js
@@ -1,30 +1,24 @@
-function getInputValue(inputId) {
-    var inp = document.getElementById(inputId);
-    if (!inp) {
-        return;
-    }
-    addListener(inp, 'blur', function (e) {
-        var from = getGaCookie() + location.href + ' - ' + this.id;
-        var to = this.value;
-        /*console.log("getInputValue blur from: " + from + ", to: " + to);*/
-        sendAnalyticsEvent('blur', from, to);
-    });
-}
-
-function trackClick(inputId) {
+/*
+* inputId - the id of the html field you want to track.
+*
+* event - A String that specifies the name of the event.
+* Note: Do not use the "on" prefix. For example, use "click" instead of "onclick".
+* For a list of all HTML DOM events, look at our complete HTML DOM Event Object Reference http://www.w3schools.com/jsref/dom_obj_event.asp.
+*/
+function track(inputId, event) {
     var element = document.getElementById(inputId);
     if (element) {
         if (element.addEventListener) {
             /* addEventListener is a W3 standard that is implemented in the majority of other browsers (FF, Webkit, Opera, IE9+) */
-            element.addEventListener('click', function (e) {
-                /*console.log("trackClick addEventListener send event: " + location.href + ", id: " + inputId + ", onClick");*/
-                ga('send', 'event', location.href, inputId, 'onClick');
+            element.addEventListener(event, function (e) {
+                /*console.log("trackClick addEventListener send event: " + location.href + ", id: " + inputId + ", event " + event);*/
+                ga('send', 'event', location.href, inputId, event);
             });
         } else if (element.attachEvent) {
             /* attachEvent can only be used on older trident rendering engines ( IE5+ IE5-8*) */
-            element.attachEvent('click', function (e) {
-                /*console.log("trackClick attachEvent send event: " + location.href + ", id: " + inputId + ", onClick");*/
-                ga('send', 'event', location.href, inputId, 'onClick');
+            element.attachEvent(event, function (e) {
+                /*console.log("trackClick attachEvent send event: " + location.href + ", id: " + inputId + ", event " + event);*/
+                ga('send', 'event', location.href, inputId, event);
             });
         } else {
             console.error("element does not support addEventListener or attachEvent");
