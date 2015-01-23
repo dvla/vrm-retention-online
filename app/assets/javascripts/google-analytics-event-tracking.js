1e6913ec94ded1373122fd4782267666c9358181
Scott Walker <scott.walker@valtech.co.uk>
2015-01-23 11:00:13 +0000

    US2216: Specify which fields to add onclick tracking

--- app/assets/javascripts/google-analytics-event-tracking.js
+++ app/assets/javascripts/google-analytics-event-tracking.js
@@ -1,55 +1,3 @@
-var pageStart = new Date();
-
-function addListener(element, type, callback) {
-    /*console.log(element);*/
-    if (element.addEventListener) {
-        /*console.log("addEventListener");*/
-        element.addEventListener(type, callback);
-    } else if (element.attachEvent) {
-        /*console.log("attachEvent");*/
-        element.attachEvent('on' + type, callback);
-    }
-}
-
-function getGaCookie() {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        if (/ga/.test(cookies[i].split("=")[0]) === true) {
-            return cookies[i].split("=")[1] + " - ";
-        }
-    }
-    return "";
-}
-
-function sendAnalyticsEvent(action, from, to) {
-    dateVal = new Date() - pageStart;
-    ga('send', 'event', action, from, to, dateVal);
-    /*console.log("sent to google action: " + action + ", from: " + from + ", to: " + to + ", dateVal: " + dateVal);*/
-    pageStart = new Date();
-}
-
-function loopByTag(tag, action, type) {
-    var list = document.getElementsByTagName(tag);
-    for (i = 0; i < list.length; i++) {
-        if (!type || list[i].type == type) {
-            addListener(list[i], action, function (e) {
-                var from = location.href;
-                var actionName = null;
-                var to = null;
-                if (tag == "input") {
-                    actionName = this.id || e.srcElement.id;
-                    to = getGaCookie() + document.forms[0].action;
-                    sendAnalyticsEvent(from, actionName, to);
-                } else {
-                    actionName = this.innerText || this.textContent || e.srcElement.innerText;
-                    to = getGaCookie() + this.href || e.srcElement.href;
-                    sendAnalyticsEvent(from, actionName, to);
-                }
-            });
-        }
-    }
-}
-
 function getInputValue(inputId) {
     var inp = document.getElementById(inputId);
     if (!inp) {
@@ -58,20 +6,32 @@ function getInputValue(inputId) {
     addListener(inp, 'blur', function (e) {
         var from = getGaCookie() + location.href + ' - ' + this.id;
         var to = this.value;
+        /*console.log("getInputValue blur from: " + from + ", to: " + to);*/
         sendAnalyticsEvent('blur', from, to);
     });
 }
-loopByTag('a', 'click');
-loopByTag('button', 'click');
-loopByTag('input', 'click', 'submit');
-loopByTag('input', 'click', 'button');
-loopByTag('input', 'click', 'checkbox');
-/*
-loopByTag('input','select','text');
-loopByTag('input','blur','text');
-*/
-/* On on blur, if field value != "" then sendAnalyticsEvent  */
 
-
-/*The line below would post the value of any textbox with id "postcode" to google analytics when it loses focus (regardless of validation)*/
-/*getInputValue('postcode');*/
+function trackClick(inputId) {
+    var element = document.getElementById(inputId);
+    if (element) {
+        if (element.addEventListener) {
+            /* addEventListener is a W3 standard that is implemented in the majority of other browsers (FF, Webkit, Opera, IE9+) */
+            element.addEventListener('click', function (e) {
+                /*console.log("trackClick addEventListener send event: " + location.href + ", id: " + inputId + ", onClick");*/
+                ga('send', 'event', location.href, inputId, 'onClick');
+            });
+        } else if (element.attachEvent) {
+            /* attachEvent can only be used on older trident rendering engines ( IE5+ IE5-8*) */
+            element.attachEvent('click', function (e) {
+                /*console.log("trackClick attachEvent send event: " + location.href + ", id: " + inputId + ", onClick");*/
+                ga('send', 'event', location.href, inputId, 'onClick');
+            });
+        } else {
+            console.error("element does not support addEventListener or attachEvent");
+            return false;
+        }
+    } else {
+        console.error("element id: " + inputId + " not found on page");
+        return false;
+    }
+}
