797bdbb86b9e6f0f8e6e6eeec0478bb47b8bb585
Scott Walker <scott.walker@valtech.co.uk>
2015-01-23 08:47:29 +0000

    US2216: fixes to broken javascript semi-colons, var decl etc according to jsfiddle. Moved code to a .js file in assets instead of in the .scala.html. Declare script in the footer (seems to only work when declared after the ids that it looks for)

--- app/assets/javascripts/google-analytics-event-tracking.js
+++ app/assets/javascripts/google-analytics-event-tracking.js
@@ -1,78 +1,77 @@
 var pageStart = new Date();
 
 function addListener(element, type, callback) {
-    if (element.addEventListener) element.addEventListener(type, callback);
-    else if (element.attachEvent) element.attachEvent('on' + type, callback);
+    /*console.log(element);*/
+    if (element.addEventListener) {
+        /*console.log("addEventListener");*/
+        element.addEventListener(type, callback);
+    } else if (element.attachEvent) {
+        /*console.log("attachEvent");*/
+        element.attachEvent('on' + type, callback);
+    }
 }
 
 function getGaCookie() {
-    var cookies = document.cookie.split(';')
-    for(var i=0;i<cookies.length;i++) {
-        if(/ga/.test(cookies[i].split("=")[0])==true){
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        if (/ga/.test(cookies[i].split("=")[0]) === true) {
             return cookies[i].split("=")[1] + " - ";
         }
     }
     return "";
 }
 
-function sendAnalyticsEvent(action,from,to) {
+function sendAnalyticsEvent(action, from, to) {
     dateVal = new Date() - pageStart;
-    ga('send','event',action,from,to,dateVal);
-    console.log("sent to google action: " + action + ", from: " + from + ", to: " + to + ", dateVal: " + dateVal);
+    ga('send', 'event', action, from, to, dateVal);
+    /*console.log("sent to google action: " + action + ", from: " + from + ", to: " + to + ", dateVal: " + dateVal);*/
     pageStart = new Date();
 }
 
-function loopByTag(tag,action,type) {
+function loopByTag(tag, action, type) {
     var list = document.getElementsByTagName(tag);
-    for (i=0;i<list.length;i++){
-        if(!type||list[i].type==type) {
-            addListener(list[i], action, function(e) {
+    for (i = 0; i < list.length; i++) {
+        if (!type || list[i].type == type) {
+            addListener(list[i], action, function (e) {
                 var from = location.href;
-                if(tag=="input"){
-                    var actionName = this.value||e.srcElement.value;
-                    var to = getGaCookie() + document.forms[0].action;
+                var actionName = null;
+                var to = null;
+                if (tag == "input") {
+                    actionName = this.id || e.srcElement.id;
+                    to = getGaCookie() + document.forms[0].action;
+                    sendAnalyticsEvent(from, actionName, to);
                 } else {
-                    var actionName = this.innerText||this.textContent||e.srcElement.innerText;
-                    var to = getGaCookie() + this.href||e.srcElement.href;
+                    actionName = this.innerText || this.textContent || e.srcElement.innerText;
+                    to = getGaCookie() + this.href || e.srcElement.href;
+                    sendAnalyticsEvent(from, actionName, to);
                 }
-                sendAnalyticsEvent(from,actionName,to);
-            })
+            });
         }
     }
 }
 
 function getInputValue(inputId) {
     var inp = document.getElementById(inputId);
-    if(!inp){return}
-    addListener(inp,'blur',function(e){
-        var from = getGaCookie() + location.href + ' - ' + this.id;
-        var to = this.value
-        sendAnalyticsEvent('blur',from,to);
-    })
-}
-
-function virtualPageView() {
-    var spans = document.getElementsByTagName('span');
-    for(i=0;i<spans.length;i++) {
-        if(/nino-help/gim.test(spans[i].outerHTML)){
-            addListener(spans[i],'click',function(e){
-                var from =  + location.href + ' - ' + this.id;
-                var to = getGaCookie() + "/nino-help";
-                ga('send', 'pageview', 'nino-help');
-            })
-        }
-        if(/dln-help/gim.test(spans[i].outerHTML)){
-            addListener(spans[i],'click',function(e){
-                var from =  + location.href + ' - ' + this.id;
-                var to = getGaCookie() + "/dln-help";
-                ga('send', 'pageview', 'dln-help');
-            })
-        }
+    if (!inp) {
+        return;
     }
+    addListener(inp, 'blur', function (e) {
+        var from = getGaCookie() + location.href + ' - ' + this.id;
+        var to = this.value;
+        sendAnalyticsEvent('blur', from, to);
+    });
 }
+loopByTag('a', 'click');
+loopByTag('button', 'click');
+loopByTag('input', 'click', 'submit');
+loopByTag('input', 'click', 'button');
+loopByTag('input', 'click', 'checkbox');
+/*
+loopByTag('input','select','text');
+loopByTag('input','blur','text');
+*/
+/* On on blur, if field value != "" then sendAnalyticsEvent  */
 
-loopByTag('a','click');
-loopByTag('button','click');
-loopByTag('input','click','submit');
-loopByTag('input','click','button');
-virtualPageView();
+
+/*The line below would post the value of any textbox with id "postcode" to google analytics when it loses focus (regardless of validation)*/
+/*getInputValue('postcode');*/
