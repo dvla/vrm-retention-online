6995fd0d27cc06a13c58f4fc224f78db1174e3c1
Scott Walker <scott.walker@valtech.co.uk>
2015-01-23 19:09:03 +0000

    US2216: track when the summary info is expanded

--- app/views/vrm_retention/Confirm.scala
+++ app/views/vrm_retention/Confirm.scala
@@ -5,6 +5,7 @@ import models.CacheKeyPrefix
 object Confirm {
 
   final val KeeperEmailMaxLength = 254
+  final val SummaryId = "summary"
   final val ConfirmId = "confirm"
   final val ExitId = "exit"
   final val KeeperEmailId = "keeper-email"
