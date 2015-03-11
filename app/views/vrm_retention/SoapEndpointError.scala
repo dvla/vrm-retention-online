b49eda332d6d416db14d5b3a8d510b89e252658d
Scott Walker <scott.walker@valtech.co.uk>
2015-03-11 14:57:30 +0000

    US2679: link text on failure pages for V317

---
+++ app/views/vrm_retention/SoapEndpointError.scala
@@ -0,0 +1,7 @@
+package views.vrm_retention
+
+object SoapEndpointError {
+
+  final val ExitId = "exit"
+  final val `v317-id` = "v317"
+}
--- app/views/vrm_retention/VehicleLookupFailure.scala
+++ app/views/vrm_retention/VehicleLookupFailure.scala
@@ -5,4 +5,5 @@ object VehicleLookupFailure {
   final val VehicleLookupId = "vehicle-lookup"
   final val BeforeYouStartId = "before-you-start"
   final val ExitId = "exit"
+  final val `v317-id` = "v317"
 }
