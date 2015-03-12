f64b0e4150072233204e0b3ce6bb53d2bfcdb8ef
Scott Walker <scott.walker@valtech.co.uk>
2015-03-12 16:04:13 +0000

    DE396: all the business cookies need to have a 7 day expiry

--- app/utils/helpers/RetentionCookieFlags.scala
+++ app/utils/helpers/RetentionCookieFlags.scala
@@ -4,14 +4,17 @@ import com.google.inject.Inject
 import play.api.mvc.Cookie
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieFlags
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.RichCookie
+import views.vrm_retention.BusinessChooseYourAddress.BusinessChooseYourAddressCacheKey
 import views.vrm_retention.BusinessDetails.BusinessDetailsCacheKey
 import views.vrm_retention.ConfirmBusiness.StoreBusinessDetailsCacheKey
+import views.vrm_retention.EnterAddressManually.EnterAddressManuallyCacheKey
+import views.vrm_retention.SetupBusinessDetails.SetupBusinessDetailsCacheKey
 
 final class RetentionCookieFlags @Inject()()(
   config2: Config) extends CookieFlags {
 
   override def applyToCookie(cookie: Cookie, key: String): Cookie =
-    if (List(StoreBusinessDetailsCacheKey, BusinessDetailsCacheKey).contains(key)) {
+    if (List(StoreBusinessDetailsCacheKey, BusinessDetailsCacheKey, BusinessChooseYourAddressCacheKey, SetupBusinessDetailsCacheKey, EnterAddressManuallyCacheKey).contains(key)) {
       cookie.
         withSecure(config2.secureCookies).
         withMaxAge(config2.storeBusinessDetailsMaxAge)
