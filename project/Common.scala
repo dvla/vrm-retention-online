3bff436a79a3e1fe5935ec77db520f05cddcaeea
Scott Walker <scott.walker@valtech.co.uk>
2015-03-17 13:52:30 +0000

    New development version 1.11-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt.Keys.version
 import sbt._
 
 object Common {
-  val versionString = "1.10"
+  val versionString = "1.11-SNAPSHOT"
   val scalaVersionString = "2.10.3"
   val organisationString = "dvla"
   val organisationNameString = "Driver & Vehicle Licensing Agency"
