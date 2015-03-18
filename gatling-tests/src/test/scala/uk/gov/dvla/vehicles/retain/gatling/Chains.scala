ea18d5057970d31fcf70543090188ff07e89c3a9
Scott Walker <scott.walker@valtech.co.uk>
2015-03-18 12:18:23 +0000

    Refactor: remove unused .gif and .png

--- gatling-tests/src/test/scala/uk/gov/dvla/vehicles/retain/gatling/Chains.scala
+++ gatling-tests/src/test/scala/uk/gov/dvla/vehicles/retain/gatling/Chains.scala
@@ -70,24 +70,8 @@ class Chains(data: RecordSeqFeederBuilder[String]) {
           """If-None-Match""" -> """0464ba08d53d88645ca77f9907c082c8c10d563b"""))
       )
       .exec(
-        http("icon-tick-green.gif")
-          .get( s"""/assets/images/icon-tick-green.gif""")
-      )
-      .exec(
-        http("open-government-licence-974ebd75112cb480aae1a55ae4593c67.png")
-          .get( s"""/assets/images/open-government-licence-974ebd75112cb480aae1a55ae4593c67.png""")
-      )
-      .exec(
-        http("payMock.png")
-          .get( s"""/assets/images/payMock.png""")
-      )
-      .exec(
         http("pdf-icon-1.png")
           .get( s"""/assets/images/pdf-icon-1.png""")
-      )
-      .exec(
-        http("progress-tick.png")
-          .get( s"""/assets/images/progress-tick.png""")
       )
 
       /* javascript */
