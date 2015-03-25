d1908b1ba9160c4b73ee84d666cb8c8f457d80fd
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-03-25 14:41:39 +0000

    Fix the exception cases for the access logging

--- app/composition/LoggerLikeBinding.scala
+++ app/composition/LoggerLikeBinding.scala
@@ -1,11 +1,16 @@
 package composition
 
 import com.google.inject.name.Names
+import uk.gov.dvla.vehicles.presentation.common
+import common.filters.{AccessLoggingConfig, DefaultAccessLoggingConfig}
 import com.tzavellas.sse.guice.ScalaModule
 import play.api.{Logger, LoggerLike}
 import uk.gov.dvla.vehicles.presentation.common.filters.AccessLoggingFilter._
 
 final class LoggerLikeBinding extends ScalaModule {
 
-  def configure() = bind[LoggerLike].annotatedWith(Names.named(AccessLoggerName)).toInstance(Logger("dvla.common.AccessLogger"))
+  def configure() = {
+    bind[LoggerLike].annotatedWith(Names.named(AccessLoggerName)).toInstance(Logger("dvla.common.AccessLogger"))
+    bind[AccessLoggingConfig].toInstance(new DefaultAccessLoggingConfig())
+  }
 }
