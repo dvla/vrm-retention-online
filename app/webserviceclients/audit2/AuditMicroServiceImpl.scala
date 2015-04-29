a93d3187ff6889f964ff089222afda614187a813
Scott Walker <scott.walker@valtech.co.uk>
2015-04-29 08:52:22 +0100

    Hotfix: make sure we are not logging audit service messages

--- app/webserviceclients/audit2/AuditMicroServiceImpl.scala
+++ app/webserviceclients/audit2/AuditMicroServiceImpl.scala
@@ -1,7 +1,6 @@
 package webserviceclients.audit2
 
 import com.google.inject.Inject
-import play.api.Logger
 import play.api.Play.current
 import play.api.libs.json.Json
 import play.api.libs.ws.{WS, WSResponse}
@@ -16,7 +15,6 @@ final class AuditMicroServiceImpl @Inject()(
     val endPoint: String = s"${config.auditMicroServiceUrlBase}/audit/v1"
     val requestAsJson = Json.toJson(request)
 
-    Logger.debug(s"Calling audit micro-service with request $request")
     WS.url(endPoint).
       withRequestTimeout(config.auditMsRequestTimeout). // Timeout is in milliseconds
       post(requestAsJson)
