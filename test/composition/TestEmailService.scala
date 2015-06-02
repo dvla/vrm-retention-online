45932e4473bcacb5ef989379c810cedafb84e1bb
Luchesar Cekov <luchesar@gmail.com>
2015-06-02 15:29:58 +0100

    US3158 fix failing unit tests related to missing email service mock stubbing

--- test/composition/TestEmailService.scala
+++ test/composition/TestEmailService.scala
@@ -2,11 +2,27 @@ package composition
 
 import com.tzavellas.sse.guice.ScalaModule
 import email.RetainEmailService
+import models.{BusinessDetailsModel, ConfirmFormModel, EligibilityModel}
 import org.scalatest.mock.MockitoSugar
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel
 
 final class TestEmailService extends ScalaModule with MockitoSugar {
 
   val stub = mock[RetainEmailService]
+  when(stub.emailRequest(
+    any[String],
+    any[VehicleAndKeeperDetailsModel],
+    any[EligibilityModel],
+    any[String],
+    any[String],
+    any[String],
+    any[Option[ConfirmFormModel]],
+    any[Option[BusinessDetailsModel]],
+    any[Boolean],
+    any[String]
+  )).thenReturn(None)
 
   def configure() = bind[RetainEmailService].toInstance(stub)
 }
