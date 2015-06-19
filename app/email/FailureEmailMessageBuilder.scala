d8b13f48686db27df7717dc292c15ffddb3fff70
Chris Rudd <chris.rudd@valtech.co.uk>
2015-06-19 12:56:05 +0100

    US3112:  Correct the formatting of the failure email.

--- app/email/FailureEmailMessageBuilder.scala
+++ app/email/FailureEmailMessageBuilder.scala
@@ -1,9 +1,6 @@
 package email
 
-        import java.text.SimpleDateFormat
-
-        import org.joda.time.{DateTimeZone, Instant}
-        import play.api.i18n.Messages
+import play.api.i18n.Messages
 
 /**
  * The email message builder class will create the contents of the message. override the buildHtml and buildText
@@ -11,76 +8,80 @@ package email
  *
  */
 
-        object FailureEmailMessageBuilder {
-        import uk.gov.dvla.vehicles.presentation.common.services.SEND.Contents
+object FailureEmailMessageBuilder {
+  import uk.gov.dvla.vehicles.presentation.common.services.SEND.Contents
 
-        def buildWith: Contents = {
+def buildWith: Contents = {
         Contents(
-        buildHtml,
-        buildText
-        )
+                buildHtml,
+                buildText
+                )
         }
 
 private def buildHtml: String =
-        s"""
-        |<html>
-|<head>
-|</head>
-        |<style>
-|p {
-        |  line-height: 200%;
-        |}
-        |ul { list-style: none; padding: 0; margin:0 0 32px 0;}
-        |li { margin-bottom: 8px}
-        |li > ul {
-        |	margin: 16px 0 0 16px;
-        |}
-        |</style>
-        |</head>
-        |<body>
-|
-        |<p>
-|	<strong>
-|  ${Messages("email.failure.line1")}
-        |  ${Messages("email.failure.line2")}
-        | </strong>
-        |</p>
-        |
-        |<p>
-|  ${Messages("email.failure.line3")}
-        |  ${Messages("email.failure.line4")}
-        |  ${Messages("email.failure.line5")}
-        |  ${Messages("email.failure.line6")}
-        |  ${Messages("email.failure.line7")}
-        |  ${Messages("email.failure.line8")}
-        |  ${Messages("email.failure.line9")}
-        |  ${Messages("email.failure.line10")}
-        |  ${Messages("email.failure.line11")}
-        |  ${Messages("email.failure.line12")}
-        |  ${Messages("email.failure.line14")}
-        |  ${Messages("email.failure.line15")}
-        |  ${Messages("email.failure.line16")}
-        |</p>
-        """.stripMargin
+s"""
+   |<html>
+   |<head>
+   |</head>
+   |<style>
+   |p {
+   |  line-height: 200%;
+   |}
+   |ul { list-style: none; padding: 0; margin:0 0 32px 0;}
+   |li { margin-bottom: 8px}
+   |li > ul {
+   |	margin: 16px 0 0 16px;
+   |}
+   |</style>
+   |</head>
+   |<body>
+   |
+   |<p>
+   |	<strong>
+   |    <ul>
+   |      <li>${Messages("email.failure.line1")}</li>
+   |     <li>${Messages("email.failure.line2")}</li>
+   |   </ul>
+   | </strong>
+   |</p>
+   |
+   |<p>
+   |<ul>
+   |  <li>${Messages("email.failure.line3")}</li>
+   |  <li>${Messages("email.failure.line4")}</li>
+   |  <li>${Messages("email.failure.line5")}</li>
+   |  <li>${Messages("email.failure.line6")}</li>
+   |  <li>${Messages("email.failure.line7")}</li>
+   |  <li>${Messages("email.failure.line8")}</li>
+   |  <li>${Messages("email.failure.line9")}</li>
+   |  <li>${Messages("email.failure.line10")}</li>
+   |  <li>${Messages("email.failure.line11")}</li>
+   |  <li>${Messages("email.failure.line12")}</li>
+   |  <li>${Messages("email.failure.line14")}</li>
+   |  <li>${Messages("email.failure.line15")}</li>
+   |  <li>${Messages("email.failure.line16")}</li>
+   |</ul>
+   |</p>
+""".stripMargin
 
 private def buildText: String =
-        s"""
-        |${Messages("email.failure.line1")}
-        |${Messages("email.failure.line2")}
-        |${Messages("email.failure.line3")}
-        |${Messages("email.failure.line4")}
-        |${Messages("email.failure.line5")}
-        |${Messages("email.failure.line6")}
-        |${Messages("email.failure.line7")}
-        |${Messages("email.failure.line8")}
-        |${Messages("email.failure.line9")}
-        |${Messages("email.failure.line10")}
-        |${Messages("email.failure.line11")}
-        |${Messages("email.failure.line12")}
-        |${Messages("email.failure.line13")}
-        |${Messages("email.failure.line14")}
-        |${Messages("email.failure.line15")}
-        |${Messages("email.failure.line16")}
-        """.stripMargin
+s"""
+   |${Messages("email.failure.line1")}
+    |${Messages("email.failure.line2")}
+    |${Messages("email.failure.line3")}
+    |${Messages("email.failure.line4")}
+    |${Messages("email.failure.line5")}
+    |${Messages("email.failure.line6")}
+    |${Messages("email.failure.line7")}
+    |${Messages("email.failure.line8")}
+    |${Messages("email.failure.line9")}
+    |${Messages("email.failure.line10")}
+    |${Messages("email.failure.line11")}
+    |${Messages("email.failure.line12")}
+    |${Messages("email.failure.line13")}
+    |${Messages("email.failure.line14")}
+    |${Messages("email.failure.line15")}
+    |${Messages("email.failure.line16")}
+""".stripMargin
 
-        }
+}
