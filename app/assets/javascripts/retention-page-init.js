3c8318a48b2c408a1346981f184ea53fb7cbba75
Oliver Jenkins <github@oliverjenkins.com>
2015-09-30 17:47:54 +0100

    US3817 Implimented summaryWrapper widget on /confirm route

--- app/assets/javascripts/retention-page-init.js
+++ app/assets/javascripts/retention-page-init.js
@@ -5,15 +5,6 @@ define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pag
         init: function() {
             // Call initAll on the pageInit object to run all the common js in vehicles-presentation-common
             pageInit.initAll();
-
-            // If JS enabled hide summary details
-            $('.details').hide();
-
-            // Summary details toggle
-            $('.summary').on('click', function() {
-                $(this).siblings().toggle();
-                $(this).toggleClass('active');
-            });
         }
     }
 });
