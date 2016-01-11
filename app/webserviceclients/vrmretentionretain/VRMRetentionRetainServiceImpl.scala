949e5ae5e2335ce1adb87c59b7c2d2b242accf74
Ian Stainer <ian.stainer@valtech.co.uk>
2016-01-11 10:20:34 +0000

    US4177 Verify that all micro-services called by the retention web app inform the HealthStats service of success or failure

--- app/webserviceclients/vrmretentionretain/VRMRetentionRetainServiceImpl.scala
+++ app/webserviceclients/vrmretentionretain/VRMRetentionRetainServiceImpl.scala
@@ -4,22 +4,49 @@ import javax.inject.Inject
 import play.api.http.Status
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.control.NonFatal
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.TrackingId
+import uk.gov.dvla.vehicles.presentation.common.services.DateService
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStats
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStatsFailure
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStatsSuccess
 
-final class VRMRetentionRetainServiceImpl @Inject()(ws: VRMRetentionRetainWebService)
+final class VRMRetentionRetainServiceImpl @Inject()(ws: VRMRetentionRetainWebService,
+                                                    dateService: DateService,
+                                                    healthStats: HealthStats)
   extends VRMRetentionRetainService {
 
   override def invoke(cmd: VRMRetentionRetainRequest,
                       trackingId: TrackingId): Future[(Int, VRMRetentionRetainResponseDto)] = {
+    import VRMRetentionRetainServiceImpl.ServiceName
+
     ws.invoke(cmd, trackingId).map { resp =>
-      if (resp.status == Status.OK)
+      if (resp.status == Status.OK) {
+        healthStats.success(HealthStatsSuccess(ServiceName, dateService.now))
         (resp.status, resp.json.as[VRMRetentionRetainResponseDto])
-      else if (resp.status == Status.INTERNAL_SERVER_ERROR)
+      }
+      else if (resp.status == Status.INTERNAL_SERVER_ERROR) {
+        val msg = s"Vrm Retention Retain micro-service call http status not OK, it was: ${resp.status}"
+        val error = new RuntimeException(msg)
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, error))
         (resp.status, resp.json.as[VRMRetentionRetainResponseDto])
-      else throw new RuntimeException(
-        s"VRM Retention Retain web service call http status not OK, it " +
-          s"was: ${resp.status}. Problem may come from either vrm-retention-retain micro-service or the VSS"
-      )
+      }
+      else {
+        val error = new RuntimeException(
+          s"VRM Retention Retain micro-service call http status not OK, it " +
+            s"was: ${resp.status}. Problem may come from either vrm-retention-retain micro-service or VSS"
+        )
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, error))
+        throw error
+      }
+    }.recover {
+      case NonFatal(e) =>
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, e))
+        throw e
     }
   }
+}
+
+object VRMRetentionRetainServiceImpl {
+  final val ServiceName = "vrm-retention-retain-microservice"
 }
