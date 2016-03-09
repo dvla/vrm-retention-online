a283270e19d13654eb093c594927e193dcdbf127
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-03-09 08:26:25 +0000

    DE847 Incorrect formatting for failure email

--- app/email/FailureEmailMessageBuilder.scala
+++ app/email/FailureEmailMessageBuilder.scala
@@ -1,6 +1,6 @@
 package email
 
-import play.api.i18n.Messages
+import play.api.i18n.{Lang, Messages}
 
 /**
  * The email message builder class will create the contents of the message. override the buildHtml and buildText
@@ -11,56 +11,42 @@ import play.api.i18n.Messages
 object FailureEmailMessageBuilder {
   import uk.gov.dvla.vehicles.presentation.common.services.SEND.Contents
 
-  def buildWith: Contents = {
-    Contents(buildHtml, buildText)
+  def buildWith(implicit lang: Lang): Contents = Contents(buildHtml, buildText)
+
+  private def buildHtml(implicit lang: Lang): String = {
+    val pMargin = "style=\"margin: 16px 0 0 32px\""
+    s"""
+       |<html>
+       |<head>
+       |</head>
+       |<body>
+       |
+       |<p $pMargin>
+       |  <strong>
+       |    ${Messages("email.failure.line1")}<br>
+       |    ${Messages("email.failure.line2")}
+       |  </strong>
+       |</p>
+       |
+       |<p>
+       |  <p $pMargin>${Messages("email.failure.line3")}</p>
+       |  <p $pMargin>${Messages("email.failure.line4")}</p>
+       |  <p $pMargin>${Messages("email.failure.line5")}</p>
+       |  <p $pMargin>${Messages("email.failure.line6")}</p>
+       |  <p $pMargin>${Messages("email.failure.line7")}<br>
+       |  ${Messages("email.failure.line8")}<br>
+       |  ${Messages("email.failure.line9")}</p>
+       |  <p $pMargin>${Messages("email.failure.line10")}</p>
+       |  <p $pMargin>${Messages("email.failure.line11")}<br>
+       |  ${Messages("email.failure.line12")}<br>
+       |  ${Messages("email.failure.line13")}</p>
+       |  <p $pMargin>${Messages("email.failure.line14")}</p>
+       |</p>
+    """.stripMargin
   }
 
-  private def buildHtml: String =
-  s"""
-     |<html>
-     |<head>
-     |</head>
-     |<style>
-     |p {
-     |  line-height: 200%;
-     |}
-     |ul { list-style: none; padding: 0; margin:0 0 32px 0;}
-     |li { margin-bottom: 8px}
-     |li > ul {
-     |	margin: 16px 0 0 16px;
-     |}
-     |</style>
-     |</head>
-     |<body>
-     |
-     |<p>
-     |	<strong>
-     |    <ul>
-     |      <li>${Messages("email.failure.line1")}</li>
-     |     <li>${Messages("email.failure.line2")}</li>
-     |   </ul>
-     | </strong>
-     |</p>
-     |
-     |<p>
-     |<ul>
-     |  <li>${Messages("email.failure.line3")}</li>
-     |  <li>${Messages("email.failure.line4")}</li>
-     |  <li>${Messages("email.failure.line5")}</li>
-     |  <li>${Messages("email.failure.line6")}</li>
-     |  <li>${Messages("email.failure.line7")}</li>
-     |  <li>${Messages("email.failure.line8")}</li>
-     |  <li>${Messages("email.failure.line9")}</li>
-     |  <li>${Messages("email.failure.line10")}</li>
-     |  <li>${Messages("email.failure.line11")}</li>
-     |  <li>${Messages("email.failure.line12")}</li>
-     |  <li>${Messages("email.failure.line14")}</li>
-     |</ul>
-     |</p>
-  """.stripMargin
-
-  private def buildText: String =
-  s"""
+  private def buildText(implicit lang: Lang): String =
+    s"""
       |${Messages("email.failure.line1")}
       |${Messages("email.failure.line2")}
       |${Messages("email.failure.line3")}
