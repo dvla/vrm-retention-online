996303520bd6e2a373e5fdc69f0b9c71ecffd3cb
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2016-03-21 12:42:53 +0000

    DE849 WEBCHAT postcode not being populated in agent view

--- app/views/vrm_retention/LiveAgent.scala
+++ app/views/vrm_retention/LiveAgent.scala
@@ -5,6 +5,6 @@ object LiveAgent {
   final val ServiceId = "Service"
   final val VrmId = "VRM"
   final val V5Id = "V5 Document Reference"
-  final val PostcodeId = "Keeper postcode"
+  final val PostcodeId = "Keeper Postcode"
   final val FailureCodeId = "Failure"
 }
