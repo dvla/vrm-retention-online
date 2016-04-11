f2f0f031509c0f49620c194b4c4edb8655f539b2
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-04-11 08:07:49 +0100

    VMPRCI-4259 Update content of vehicle lookup failure page

--- acceptance-tests/src/test/scala/pages/VehicleNotFoundPageSteps.scala
+++ acceptance-tests/src/test/scala/pages/VehicleNotFoundPageSteps.scala
@@ -30,7 +30,9 @@ class VehicleNotFoundPageSteps(implicit webDriver: EventFiringWebDriver)
 
   def `has 'not found' message` = {
     pageSource should include("Unable to find vehicle record")
-    pageSource should include("The V5C document reference number and/or the vehicle registration number entered is either not valid or does not come from the most recent V5C issued for this vehicle.")
+    pageSource should include("The vehicle registration number and/or the V5C document reference " +
+      "number entered is either incorrect or does not come from the latest V5C issued for this vehicle.")
+    pageSource should include("To contact us:")
     pageSource should not include "This registration number cannot be retained online"
     pageSource should not include "complete and submit a V317 form"
     this
@@ -38,7 +40,9 @@ class VehicleNotFoundPageSteps(implicit webDriver: EventFiringWebDriver)
 
   def `has 'doc ref mismatch' message` = {
     pageSource should include("Unable to find vehicle record")
-    pageSource should include("The V5C document reference number entered is either not valid or does not come from the most recent V5C issued for this vehicle.")
+    pageSource should include("The vehicle registration number and/or the V5C document reference " +
+      "number entered is either incorrect or does not come from the latest V5C issued for this vehicle.")
+    pageSource should not include "To contact us:"
     pageSource should not include "This registration number cannot be assigned online"
     pageSource should not include "complete and submit a V317 form"
     this
