f21bc61262351f23d055a1741994206579ac6f6c
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2016-06-30 10:57:43 +0100

    VMPRCI-4424 Refactor part of ErrorStrategy

--- app/utils/helpers/ErrorStrategy.scala
+++ app/utils/helpers/ErrorStrategy.scala
@@ -4,26 +4,14 @@ import com.google.inject.Inject
 import com.google.inject.name.Named
 import controllers.routes
 import play.api.LoggerLike
-import play.api.mvc.RequestHeader
-import play.api.mvc.Results.Redirect
-import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.ErrorStrategyBase
 import uk.gov.dvla.vehicles.presentation.common.filters.AccessLoggingFilter.AccessLoggerName
 import uk.gov.dvla.vehicles.presentation.common.filters.ClfEntryBuilder
-import uk.gov.dvla.vehicles.presentation.common.LogFormats.DVLALogger
 import uk.gov.dvla.vehicles.presentation.common.services.DateService
-import uk.gov.dvla.vehicles.presentation.common.utils.helpers.CookieHelper
 
 class ErrorStrategy @Inject()(clfEntryBuilder: ClfEntryBuilder,
                               @Named(AccessLoggerName) accessLogger: LoggerLike,
-                              dateService: DateService )(implicit clientSideSessionFactory: ClientSideSessionFactory)
-  extends ErrorStrategyBase(clfEntryBuilder, clfEntry => accessLogger.info(clfEntry), accessLogger, dateService)
-  with DVLALogger {
-
-  protected override def sessionExceptionResult(request: RequestHeader) = {
-    CookieHelper.discardAllCookies(routes.BeforeYouStart.present)(request)
-  }
-
-  protected override def errorPageResult(exceptionDigest: String) =
-    Redirect(routes.Error.present(exceptionDigest))
-}
+                              dateService: DateService,
+                              exceptionDigest: String)
+  extends ErrorStrategyBase(clfEntryBuilder, clfEntry => accessLogger.info(clfEntry), accessLogger, dateService,
+    routes.BeforeYouStart.present, routes.Error.present(exceptionDigest))
