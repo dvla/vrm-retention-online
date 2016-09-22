8eb2313b806501f82acb36d57e915e8b188a1a31
Ian Stainer <ian.stainer@valtech.co.uk>
2016-09-22 20:59:05 +0100

    VMPRCI-4513 temp change to fix the build

--- app/controllers/Version.scala
+++ app/controllers/Version.scala
@@ -7,14 +7,14 @@ import common.webserviceclients.addresslookup.ordnanceservey.OrdnanceSurveyConfi
 import common.webserviceclients.vehicleandkeeperlookup.VehicleAndKeeperLookupConfig
 import utils.helpers.Config
 
-class Version @Inject()(vehicleAndKeeperConfig: VehicleAndKeeperLookupConfig,
-                        osAddressLookupConfig: OrdnanceSurveyConfig,
+class Version @Inject()(//vehicleAndKeeperConfig: VehicleAndKeeperLookupConfig,
+//                        osAddressLookupConfig: OrdnanceSurveyConfig,
                         config: Config) extends common.controllers.Version(
   config.auditMicroServiceUrlBase + Suffix,
   config.emailServiceMicroServiceUrlBase + Suffix,
-  osAddressLookupConfig.baseUrl + Suffix,
+//  osAddressLookupConfig.baseUrl + Suffix,
   config.paymentSolveMicroServiceUrlBase + Suffix,
-  vehicleAndKeeperConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + Suffix,
+//  vehicleAndKeeperConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + Suffix,
   config.vrmRetentionEligibilityMicroServiceUrlBase + Suffix,
   config.vrmRetentionRetainMicroServiceUrlBase + Suffix
 )
